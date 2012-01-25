0b26801963953d4a554ae47f77a5ddd374c22959
Christos Psaltis <cpsaltis@unweb.me>
2012-01-25 22:46:20 +0200

    remove unused views, templates and css

--- src/mist/io/__init__.py
+++ src/mist/io/__init__.py
@@ -17,36 +17,26 @@ def main(global_config, **settings):
                     route_name='machines',
                     renderer='templates/machines.pt')
 
-    config.add_route('disks', '/disks')
-    config.add_view('mist.io.views.disks',
-                    route_name='disks',
-                    renderer='templates/disks.pt')
-
     config.add_route('images', '/images')
     config.add_view('mist.io.views.images',
                     route_name='images',
                     renderer='templates/images.pt')
 
-    config.add_route('networks', '/networks')
-    config.add_view('mist.io.views.networks',
-                    route_name='networks',
-                    renderer='templates/network.pt')
-
     config.add_route('list_machines', '/backends/{backend}/machines/list')
     config.add_view('mist.io.views.list_machines',
                     route_name='list_machines')
-    
+
     config.add_route('list_sizes', '/backends/{backend}/sizes/list')
     config.add_view('mist.io.views.list_sizes',
                     route_name='list_sizes')
 
     config.add_route('list_images', '/backends/{backend}/images/list')
     config.add_view('mist.io.views.list_images',
-                    route_name='list_images')    
+                    route_name='list_images')
 
     config.add_route('list_locations', '/backends/{backend}/locations/list')
     config.add_view('mist.io.views.list_locations',
-                    route_name='list_locations')    
+                    route_name='list_locations')
 
     config.add_route('create', '/backends/{backend}/machines/create')
     config.add_view('mist.io.views.create_machine',
@@ -55,7 +45,7 @@ def main(global_config, **settings):
     config.add_route('start', '/backends/{backend}/machines/{machine}/start')
     config.add_view('mist.io.views.start_machine',
                     route_name='start')
-    
+
     config.add_route('stop', '/backends/{backend}/machines/{machine}/stop')
     config.add_view('mist.io.views.stop_machine',
                     route_name='stop')
@@ -75,6 +65,6 @@ def main(global_config, **settings):
     config.add_route('destroy', '/backends/{backend}/machines/{machine}/destroy')
     config.add_view('mist.io.views.destroy_machine',
                     route_name='destroy')
-    
+
 
     return config.make_wsgi_app()
