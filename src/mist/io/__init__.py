81e1c73c45fdac03bf260aa17aa9c1f2699f0388
Christos Psaltis <cpsaltis@unweb.me>
2012-02-07 19:54:17 +0200

    Use decorators for views and move routes and views to core

--- src/mist/io/__init__.py
+++ src/mist/io/__init__.py
@@ -6,76 +6,13 @@ def main(global_config, **settings):
     config = Configurator(root_factory=Root, settings=settings)
 
     config.add_static_view('static', 'mist.io:static')
-
     config.add_route('home', '/')
-    config.add_view('mist.io.views.home',
-                    route_name='home',
-                    request_method='GET',
-                    renderer='templates/home.pt')
-
     config.add_route('machines', '/backends/{backend}/machines')
-    config.add_view('mist.io.views.list_machines',
-                    route_name='machines',
-                    request_method='GET')
-    config.add_view('mist.io.views.create_machine',
-                    route_name='machines',
-                    request_method='POST')
-
     config.add_route('machine', '/backends/{backend}/machines/{machine}')
-    config.add_view('mist.io.views.reboot_machine',
-                    request_method='POST',
-                    request_param='action=reboot',
-                    route_name='machine')
-    config.add_view('mist.io.views.destroy_machine',
-                    request_method='POST',
-                    request_param='action=destroy',
-                    route_name='machine')
-    config.add_view('mist.io.views.start_machine',
-                    request_method='POST',
-                    request_param='action=start',
-                    route_name='machine')
-    config.add_view('mist.io.views.stop_machine',
-                    request_method='POST',
-                    request_param='action=stop',
-                    route_name='machine')
-
     config.add_route('metadata', '/backends/{backend}/machines/{machine}/metadata')
-    config.add_view('mist.io.views.set_metadata',
-                    request_method='POST',
-                    route_name='metadata')
-    config.add_view('mist.io.views.list_metadata',
-                    request_method='GET',
-                    route_name='metadata')
-
-    config.add_route('alerts', '/backends/{backend}/machines/{machine}/alerts')
-    config.add_view('mist.io.views.list_alerts',
-                    request_method='GET',
-                    route_name='alerts')
-    config.add_view('mist.io.views.send_alert',
-                    request_method='POST',
-                    route_name='alerts')
-
-    config.add_route('alert_settings', '/backends/{backend}/machines/{machine}/alerts/settings')
-    config.add_view('mist.io.views.list_alert_settings',
-                    request_method='GET',
-                    route_name='alert_settings')
-    config.add_view('mist.io.views.update_alert',
-                    request_method='POST',
-                    route_name='alert_settings')
-
     config.add_route('images', '/backends/{backend}/images')
-    config.add_view('mist.io.views.list_images',
-                    route_name='images',
-                    request_method='GET')
-
     config.add_route('sizes', '/backends/{backend}/sizes')
-    config.add_view('mist.io.views.list_sizes',
-                    request_method='GET',
-                    route_name='sizes')
-
     config.add_route('locations', '/backends/{backend}/locations')
-    config.add_view('mist.io.views.list_locations',
-                    request_method='GET',
-                    route_name='locations')
 
+    config.scan()
     return config.make_wsgi_app()
--- src/mist/io/views.py
+++ src/mist/io/views.py
@@ -7,19 +7,6 @@ from libcloud.compute.deployment import MultiStepDeployment
 from mist.io.config import BACKENDS
 
 
-def home(request):
-    '''Fill in an object with backend data, taken from config.py'''
-    backends = []
-    for b in BACKENDS:
-        backends.append({'id'           : b['id'],
-                         'title'        : b['title'],
-                         'provider'     : b['provider'],
-                         'poll_interval': b['poll_interval'],
-                         'status'       : 'off',
-                        })
-    return {'project': 'mist.io', 'backends': backends}
-
-# cpsaltis: is this used somewhere?
 def connect(backend):
     '''Establish backend connection using the credentials specified'''
     try:
@@ -40,6 +27,21 @@ def connect(backend):
         return 0
 
 
+@view_config(route_name='home', request_method='GET', renderer='templates/home.pt')
+def home(request):
+    '''Fill in an object with backend data, taken from config.py'''
+    backends = []
+    for b in BACKENDS:
+        backends.append({'id'           : b['id'],
+                         'title'        : b['title'],
+                         'provider'     : b['provider'],
+                         'poll_interval': b['poll_interval'],
+                         'status'       : 'off',
+                        })
+    return {'project': 'mist.io', 'backends': backends}
+
+
+@view_config(route_name='machines', request_method='GET')
 def list_machines(request):
     '''List machines for a backend'''
     ret = []
@@ -72,6 +74,8 @@ def list_machines(request):
                     })
     return Response(json.dumps(ret))
 
+
+@view_config(route_name='machines', request_method='POST')
 def create_machine(request):
     '''Create a new virtual machine on the specified backend'''
     ret = []
@@ -125,6 +129,7 @@ def create_machine(request):
         return Response('Invalid backend', 404)
 
 
+@view_config(route_name='machine', request_method='POST', request_param='action=start')
 def start_machine(request):
     ret = []
     found = False
@@ -146,6 +151,7 @@ def start_machine(request):
     return Response(json.dumps(ret))
 
 
+@view_config(route_name='machine', request_method='POST', request_param='action=stop')
 def stop_machine(request):
     ret = []
     found = False
@@ -169,6 +175,7 @@ def stop_machine(request):
     return Response(json.dumps(ret))
 
 
+@view_config(route_name='machine', request_method='POST', request_param='action=reboot')
 def reboot_machine(request):
     ret = []
     found = False
@@ -190,6 +197,7 @@ def reboot_machine(request):
     return Response(json.dumps(ret))
 
 
+@view_config(route_name='machine', request_method='POST', request_param='action=destroy')
 def destroy_machine(request):
     ret = []
     found = False
@@ -211,6 +219,7 @@ def destroy_machine(request):
     return Response(json.dumps(ret))
 
 
+@view_config(route_name='metadata', request_method='GET')
 def list_metadata(request):
     '''Lists metadata for a machine, given the backend and machine id'''
     ret = []
@@ -238,6 +247,7 @@ def list_metadata(request):
     return Response(json.dumps(metadata))
 
 
+@view_config(route_name='metadata', request_method='POST')
 def set_metadata(request):
     '''Sets metadata for a machine, given the backend and machine id'''
     ret = []
@@ -273,30 +283,7 @@ def set_metadata(request):
     #example EC2: conn2.ex_create_tags(machine, {'something': 'something_something'})
 
 
-def list_alerts(request):
-    '''View alert history'''
-    #TODO: populate this
-    return True
-
-
-def send_alert(request):
-    '''Forwards an alert event'''
-    #TODO: populate this
-    return True
-
-
-def list_alert_settings(request):
-    '''List all the alert rules'''
-    #TODO: populate this
-    return True
-
-
-def update_alert(request):
-    '''Update/set and alert rule'''
-    #TODO: populate this
-    return True
-
-
+@view_config(route_name='images', request_method='GET')
 def list_images(request):
     '''List images from each backend'''
     ret = []
@@ -319,6 +306,7 @@ def list_images(request):
     return Response(json.dumps(ret))
 
 
+@view_config(route_name='sizes', request_method='GET')
 def list_sizes(request):
     '''List sizes (aka flavors) from each backend'''
     ret = []
@@ -345,6 +333,7 @@ def list_sizes(request):
     return Response(json.dumps(ret))
 
 
+@view_config(route_name='locations', request_method='GET')
 def list_locations(request):
     '''List locations from each backend'''
     ret = []
