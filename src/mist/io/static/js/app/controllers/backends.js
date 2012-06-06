2471eb4159e486af91ad9c0c0219a28805fb85a1
Christos Psaltis <cpsaltis@unweb.me>
2012-06-06 18:26:29 +0300

    Use html template for backends

--- src/mist/io/static/js/app/controllers/backends.js
+++ src/mist/io/static/js/app/controllers/backends.js
@@ -9,43 +9,29 @@ define('app/controllers/backends', [
 	function(Backend, backend_button_html) {
 		return Ember.ArrayController.extend({
 			content: [],
-			
 			// Compile and render the Backends buttons view
 			backendsButtonsView: Ember.CollectionView.create({
-			    tagName: 'div',
-			    itemViewClass: Ember.View.extend({
-			    	attributeBindings: ['data-role', 'data-rel', 'href', 'data-theme',
-			    	                    'data-inline', 'data-transition', 'data-corners',
-			    	                    'data-shadow'],
-			        href: "#edit-backend",
-			        'data-rel': "dialog",
-			        'data-theme': "c",
-			        'data-inline': "true",
-			        'data-transition': "slidedown", 
-			        'data-corners': "false",
-			        'data-shadow': "false",
-			        'data-role': "button",
-			        //class="backend-state-{{content.status}}"
-			      tagName: 'a',	
-			      template: Ember.Handlebars.compile('{{content.title}}'),
-			      didInsertElement: function(e){
-			    	  $("#backend-buttons").trigger('create');
-			      }
-			    }),
 			    contentBinding:Ember.Binding.oneWay('Mist.backendsController.content'),
+			    itemViewClass: Ember.View.extend({
+                    tagName:false,
+                    template: Ember.Handlebars.compile(backend_button_html),
+                    didInsertElement: function(e){
+                      $("#backend-buttons").trigger('create');
+                    }
+			    }),
             }),
-            
+
             machinesCount: 0,
 
 			init: function() {
 				this._super();
-				
+
 				var that = this;
 				$.getJSON('/backends', function(data) {
 					data.forEach(function(item){
 						that.pushObject(Backend.create(item));
 					});
-					
+
 					that.content.forEach(function(item){
 						item.machines.addObserver('length', function() {
 							var count = 0;
