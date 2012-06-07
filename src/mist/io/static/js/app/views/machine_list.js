7d5b4569b03dd5284e6f8a9674e885c5061b64de
Stephane Konstantaropoulos <stephane@voyageonline.co.uk>
2012-06-07 20:47:49 +0300

    machines list OK

--- src/mist/io/static/js/app/views/machine_list.js
+++ src/mist/io/static/js/app/views/machine_list.js
@@ -1,27 +1,57 @@
-define('app/views/machine_list', ['ember'],
+define('app/views/machine_list', [
+	'text!app/views/machine_list_item.html','ember'],
 	/**
 	 *
 	 * Machine List View
 	 *
 	 * @returns Class
 	 */
-	function() {
+	function(machine_list_item_html) {
 		return Ember.View.extend({
 				tagName:false,
-				didInsertElement: function(e){
+				
+				didInsertElement: function(){
 			    	
+					var that = this;
+					
+					
+					
 			    	Em.run.next(function() {
+			    		
 			    		try {
-			    			$('#machines-list').listview('refresh');
+			    			that.get('parentView').$().find("ul").listview('refresh');
 			    		} catch(e) {
 			    			try {
-				    			$('#machines-list').listview();
+			    				that.get('parentView').$().find("ul").listview();
 				    		} catch(e) {
 				    			
 				    		}	
 			    		}
+			    		that.get('parentView').$().find("input[type='checkbox']").checkboxradio();
 			        });
 			    },
+			    
+			    checkBoxClick: function(event, el){
+			    	if(event.target.tagName != 'A'){
+			        	event.stopPropagation();
+			        	if(event.target.tagName == 'INPUT'){
+			        		var $this = $(event.target);
+			        		var checked = $this.is(':checked');
+			        		if (checked) {
+			        			$this.closest('.node').append('<span class="mist-node-selected" style="display:none">mist-node-selected</span>');
+			        		} else {
+			        			$this.closest('.node').find('.mist-node-selected').remove();
+			        		}
+			        		$this.checkboxradio("refresh");
+			        	}
+			        }
+			    },
+			    
+			    init: function() {
+					this._super();
+					// cannot have template in home.pt as pt complains
+					this.set('template', Ember.Handlebars.compile(machine_list_item_html));
+				},
 	    });
 			
 	}
