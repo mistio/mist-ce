66fdcfb18ca30e357381dbdab5fbb1a48516430f
Dimitris Moraitis <dimo@unweb.me>
2012-07-10 23:01:07 +0300

    deploy with ssh key on rackspace backends

--- src/mist/io/__init__.py
+++ src/mist/io/__init__.py
@@ -1,11 +1,13 @@
 from pyramid.config import Configurator
 from mist.io.resources import Root
+from mist.io.config import KEYPAIRS
 
 def main(global_config, **settings):
     """This function returns a Pyramid WSGI application"""
     if not settings.keys():
         settings=global_config
-        
+    
+    settings['keypairs'] = KEYPAIRS
     config = Configurator(root_factory=Root, settings=settings)
     config.add_static_view('static', 'mist.io:static')
     config.add_route('home', '/')
