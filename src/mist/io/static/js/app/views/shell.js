ffe2d3115b39ebc0b9388209438b47572d79d6dc
Stéphane Konstantaropoulos <stephane@voyageonline.co.uk>
2012-08-02 01:46:49 +0300

    enable button on text input, send to selected machine

--- src/mist/io/static/js/app/views/shell.js
+++ src/mist/io/static/js/app/views/shell.js
@@ -12,8 +12,33 @@ define('app/views/shell', [
 			machineBinding: 'Mist.machine',
 
 			shell: function(){
-                this.machine.shell(this.command);
+				var machine = this.machine;
+				if(!machine){
+					Mist.backendsController.forEach(function(backend){
+						backend.machines.forEach(function(m){
+							if(m.selected && m.hasKey){
+								console.log('machine selected');
+								machine = m;
+							}
+						});
+					}); 
+				}
+				this.set('machine', machine);
+				this.machine.shell(this.command);
+				this.clear();
 			},
+			
+			clear: function(){
+				this.set('command', '');
+			},
+			
+			disabledClass: function(){
+				if(this.command && this.command.length > 0){
+					return '';
+				} else {
+					return 'ui-disabled';
+				}
+			}.property('command'),
 
 		    init: function() {
 				this._super();
