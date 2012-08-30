d1b9dfb13f0c7906d2f2fa6a9dc9f3f551f291bb
Christos Psaltis <cpsaltis@unweb.me>
2012-08-30 15:44:11 +0300

    Rename machinecaps to helpers

--- src/mist/io/helpers.py
+++ src/mist/io/helpers.py
@@ -6,16 +6,16 @@ def get_machine_actions(machine, backend):
     """Return available machine actions
 
     This depends on the backend driver
-    """                
+    """
     can_start = True
     can_stop = True
     can_destroy = True
     can_reboot = True
-    
+
     if type(backend) is RackspaceNodeDriver:
         can_start = False
         can_stop = False
-    
+
     if machine.state == 1:
         can_start = False
         can_stop = False
@@ -25,12 +25,9 @@ def get_machine_actions(machine, backend):
         can_reboot = False
     elif machine.state in (3, 4) :
         can_start = False
-        can_destroy = False 
+        can_destroy = False
         can_stop = False
         can_reboot = False
-
-
-        
 
     return {'can_stop': can_stop, \
             'can_start': can_start, \
