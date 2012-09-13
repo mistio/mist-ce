c44b140f48e068ad2c4bf88b326e103b06e334c1
Dimitris Moraitis <dimo@unweb.me>
2012-09-13 19:26:37 +0300

    add jqm css

--- src/mist/io/static/js/app.js
+++ src/mist/io/static/js/app.js
@@ -7,7 +7,7 @@ require.config({
 		jquery: 'lib/jquery-1.8.1.min',
         d3: "lib/d3.v2",
         ember: 'lib/ember-0.9.8.1.min',
-        mobile: 'lib/jquery.mobile-1.2.0-beta.1.min',
+        mobile: 'lib/jquery.mobile-1.2.0-beta.1',
 		text: 'lib/require/text',
 		mocha: 'lib/mocha',
 		chai: 'lib/chai',
