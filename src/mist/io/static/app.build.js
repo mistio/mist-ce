301c34db865a85098fba7dca6d3e4ab1bdbdf2e7
Dimitris Moraitis <dimo@unweb.me>
2012-09-16 19:34:01 +0300

    update js build

--- src/mist/io/static/app.build.js
+++ src/mist/io/static/app.build.js
@@ -1,15 +1,15 @@
 ({
     baseUrl: 'js',
     paths: {
-        jquery: 'lib/jquery-1.8.1.min',
-        d3: "lib/d3.v2",
-        ember: 'lib/ember-0.9.8.1.min',
+        jquery: 'lib/jquery-1.8.1',
+        d3: "lib/d3-2.10.1",
+        ember: 'lib/ember-0.9.8.1',
         mobile: 'lib/jquery.mobile-1.2.0-beta.1',
         text: 'lib/require/text',
         mocha: 'lib/mocha',
         chai: 'lib/chai',
         jqueryUi: "lib/jquery-ui-1.8.23.custom",
-        cubism: "lib/cubism.v1",
+        cubism: "lib/cubism-1.2.2",
     },
     out : 'build/mist.js',
     name : 'app'
