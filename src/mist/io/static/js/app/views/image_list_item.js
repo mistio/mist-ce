4cc8d6269f43694a6eda011d8144722164a56441
Christos Psaltis <cpsaltis@unweb.me>
2012-09-17 23:51:19 +0300

    Delete empty lines and convert whitespace to spaces

--- src/mist/io/static/js/app/views/image_list_item.js
+++ src/mist/io/static/js/app/views/image_list_item.js
@@ -9,38 +9,37 @@ define('app/views/image_list_item', [
 	function(image_list_item_html) {
 		return Ember.View.extend({
 				tagName:false,
-				
+
 				didInsertElement: function(){
-			    	
+
 					var that = this;
-					
+
 			    	Em.run.next(function() {
-			    		
+
 			    		try {
 			    			that.get('parentView').$().find("ul").listview('refresh');
 			    		} catch(e) {
 			    			try {
 			    				that.get('parentView').$().find("ul").listview();
 				    		} catch(e) {
-				    			
-				    		}	
+
+				    		}
 			    		}
 			        });
 			    },
-			    
+
 			    createMachine: function(){
 			    	$.mobile.changePage('#dialog-add', 'pop', true, true);
 			    	Mist.machineAddController.set("newMachineBackend", this.image.backend);
 			    	Mist.machineAddController.set("newMachineImage", this.image);
-			    	
 			    },
-			    			    
+
 			    init: function() {
 					this._super();
 					// cannot have template in home.pt as pt complains
 					this.set('template', Ember.Handlebars.compile(image_list_item_html));
 				},
 	    });
-			
+
 	}
 );
