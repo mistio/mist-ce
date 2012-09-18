85a1c715650df08d359bc95bce0f0ff1df34117d
Christos Psaltis <cpsaltis@unweb.me>
2012-09-18 16:17:51 +0300

    JSON bodies for metadata requests

--- src/mist/io/static/js/app/views/delete_tag.js
+++ src/mist/io/static/js/app/views/delete_tag.js
@@ -1,4 +1,7 @@
-define('app/views/delete_tag', ['ember'],
+define('app/views/delete_tag', [
+    'ember',
+    'jquery'
+    ],
     /**
      *
      * Delete tag view
@@ -6,27 +9,32 @@ define('app/views/delete_tag', ['ember'],
      * @returns Class
      */
     function() {
-        
+
         return Ember.View.extend({
             tagName: false,
-            
-            didInsertElement: function(e){
-            	$("a.tagButton").button();
-            },
-        
-        	deleteTag: function() {
-        		var tag = this.tag;
 
-			    this.machine.tags.removeObject(this.tag.toString());
-			    
-			    var machine = this.machine;
-			    
-			    log("tag to delete: " + tag);
-			    
-			    $.ajax({
+            didInsertElement: function(e){
+                $("a.tagButton").button();
+            },
+
+            deleteTag: function() {
+                var tag = this.tag;
+
+                this.machine.tags.removeObject(this.tag.toString());
+
+                var machine = this.machine;
+
+                log("tag to delete: " + tag);
+
+                var payload = {
+                    'tag': tag.toString()
+                };
+
+                $.ajax({
                     url: 'backends/' + machine.backend.index + '/machines/' + machine.id + '/metadata',
                     type: 'DELETE',
-                    data: tag.toString(),
+                    contentType: 'application/json',
+                    data: JSON.stringify(payload),
                     success: function(data) {
                         info('Successfully deleted tag from machine', machine.name);
                     },
@@ -34,12 +42,10 @@ define('app/views/delete_tag', ['ember'],
                         Mist.notificationController.notify('Error while deleting tag from machine ' +
                                 machine.name);
                         error(textstate, errorThrown, 'while deleting tag from machine machine', machine.name);
-        			    machine.tags.addObject(tag.toString());
+                        machine.tags.addObject(tag.toString());
                     }
                 });
-        		
-            },
-            
+            }
         });
     }
 );
--- src/mist/io/static/js/app/views/machine_tags_dialog.js
+++ src/mist/io/static/js/app/views/machine_tags_dialog.js
@@ -1,29 +1,38 @@
 define('app/views/machine_tags_dialog', [
-	'text!app/templates/machine_tags_dialog.html','ember'],
-	/**
-	 *
-	 * Machine Tags Dialog page
-	 *
-	 * @returns Class
-	 */
-	function(machine_tags_dialog_html) {
-		return Ember.View.extend({
-			tagName: false,
-	
+    'text!app/templates/machine_tags_dialog.html',
+    'ember',
+    'jquery'
+    ],
+    /**
+     *
+     * Machine Tags Dialog page
+     *
+     * @returns Class
+     */
+    function(machine_tags_dialog_html) {
+        return Ember.View.extend({
+            tagName: false,
+
             submit: function(){
-			    var tag = this.tag;
-			    
-			    var machine = Mist.machine;
-			    machine.tags.addObject(tag);
-			    
-			    log("tag to add: " + tag);
-			    $.ajax({
+                var tag = this.tag;
+
+                var machine = Mist.machine;
+                machine.tags.addObject(tag);
+
+                log("tag to add: " + tag);
+
+                var payload = {
+                    'tag': tag
+                };
+
+                $.ajax({
                     url: 'backends/' + machine.backend.index + '/machines/' + machine.id + '/metadata',
                     type: 'POST',
-                    data: tag,
+                    contentType: 'application/json',
+                    data: JSON.stringify(payload),
                     success: function(data) {
                         info('Successfully added tag to machine', machine.name);
-                        
+
                     },
                     error: function(jqXHR, textstate, errorThrown) {
                         Mist.notificationController.notify('Error while adding tag to machine ' +
@@ -32,20 +41,20 @@ define('app/views/machine_tags_dialog', [
                         machine.tags.removeObject(tag);
                     }
                 });
-		    },
-		    
-		    disabledClass : function() {
-				if (this.tag && this.tag.length > 0) {
-					return '';
-				} else {
-					return 'ui-disabled';
-				}
-			}.property('tag'),
+            },
 
-		    init: function() {
-				this._super();
-				this.set('template', Ember.Handlebars.compile(machine_tags_dialog_html));
-		    }
-		});
-	}
+            disabledClass : function() {
+                if (this.tag && this.tag.length > 0) {
+                    return '';
+                } else {
+                    return 'ui-disabled';
+                }
+            }.property('tag'),
+
+            init: function() {
+                this._super();
+                this.set('template', Ember.Handlebars.compile(machine_tags_dialog_html));
+            }
+        });
+    }
 );
