c7ef0be0a9366e9b59740ef26529484e0abd677f
Dimitris Moraitis <dimo@unweb.me>
2012-10-04 16:08:23 +0300

    create ember app on first jqm pageinit event

--- src/mist/io/static/js/app.js
+++ src/mist/io/static/js/app.js
@@ -92,195 +92,204 @@ define( 'app', [
                 Mobile,
                 cubism
                 ) {
-
-        var App = Ember.Application.create({
-
-            VERSION: '0.3-ember',
-
-            // Sets up mocha to run some integration tests
-            specsRunner: function( chai ) {
-                // Create placeholder for mocha output
-                $( document.body ).before( '<div id="mocha"></div>' );
-
-                // Setup mocha and expose chai matchers
-                window.expect = chai.expect;
-                mocha.setup('bdd');
-
-                // Load testsuite
-                require([
-                    'app/specs/templates/basic_acceptance'
-                ], function() {
-                        mocha.run().globals( [ '$', 'Ember', 'Mist' ] );
-                    }
-                );
-            },
-
-            // Constructor
-            init: function() {
-                this._super();
-
-                this.set(
-                    'backendsController',
-                    BackendsController.create()
-                );
-
-                this.set(
-                    'confirmationController',
-                    ConfirmationController.create()
-                );
-
-                this.set(
-                    'notificationController',
-                       NotificationController.create()
-                );
-
-                this.set(
-                    'machineAddController',
-                    MachineAddController.create()
-                );
-
-                this.set(
-                    'selectMachinesController',
-                    SelectMachinesController.create()
-                );
-
-                this.set(
-                    'selectImagesController',
-                    SelectImagesController.create()
-                );
-
-                // Run specs if asked
-                if ( location.hash.match( /specs/ ) ) {
-                    require( [ 'chai', 'mocha' ], this.specsRunner );
-                }
-
-                setTimeout(function(){
-                    $.mobile.changePage('#home', { transition: 'fade' });
-                }, 2000);
-
-
+                    
+        var mobileinit = false;
+        $(document).bind('pageinit', function() {
+            if (mobileinit){
+                return
             }
-        });
-
-        $(document).on( 'pagebeforeshow', '#machines', function() {
-            $('#machines-list').listview('refresh');
-        });
-
-        $(document).on( 'pagebeforeshow', '#dialog-power', function() {
-            $("#dialog-power a").button();
-        });
-
-        $(document).on( 'pagebeforeshow', '#dialog-single-power', function() {
-            $("#dialog-single-power a").button();
-        });
-
-        $(document).on( 'pagebeforeshow', '#images', function() {
-            $("#images-list").listview('refresh');
-        });
-
-        $(document).on( 'pagebeforeshow', '#single-machine', function() {
-            Mist.graphPolling = true;
-        });
-
-        $(document).on( 'pageshow', '#single-machine', function() {
-            $(".monitoring-button").button();
-        });
-
-        $(document).on( 'pagebeforehide', '#single-machine', function() {
-            Mist.graphPolling = false;
-        });
-
-        // Console toggle behavior
-        $(document).ready(function() {
-            $('#shell-return').on('click', '.command', function() {
-                var out = $(this).next('.output');
-                if (out.is(':visible')) {
-                    out.slideUp(300);
-                    $(this).parent().addClass('contracted').removeClass('expanded');
-                } else {
-                    out.slideDown(200);
-                    $(this).parent().removeClass('contracted').addClass('expanded');
+            
+            mobileinit = true;
+            
+            var App = Ember.Application.create({
+    
+                VERSION: '0.3-ember',
+    
+                // Sets up mocha to run some integration tests
+                specsRunner: function( chai ) {
+                    // Create placeholder for mocha output
+                    $( document.body ).before( '<div id="mocha"></div>' );
+    
+                    // Setup mocha and expose chai matchers
+                    window.expect = chai.expect;
+                    mocha.setup('bdd');
+    
+                    // Load testsuite
+                    require([
+                        'app/specs/templates/basic_acceptance'
+                    ], function() {
+                            mocha.run().globals( [ '$', 'Ember', 'Mist' ] );
+                        }
+                    );
+                },
+    
+                // Constructor
+                init: function() {
+                    this._super();
+    
+                    this.set(
+                        'backendsController',
+                        BackendsController.create()
+                    );
+    
+                    this.set(
+                        'confirmationController',
+                        ConfirmationController.create()
+                    );
+    
+                    this.set(
+                        'notificationController',
+                           NotificationController.create()
+                    );
+    
+                    this.set(
+                        'machineAddController',
+                        MachineAddController.create()
+                    );
+    
+                    this.set(
+                        'selectMachinesController',
+                        SelectMachinesController.create()
+                    );
+    
+                    this.set(
+                        'selectImagesController',
+                        SelectImagesController.create()
+                    );
+    
+                    // Run specs if asked
+                    if ( location.hash.match( /specs/ ) ) {
+                        require( [ 'chai', 'mocha' ], this.specsRunner );
+                    }
+    
+                    setTimeout(function(){
+                        $.mobile.changePage('#home', { transition: 'fade' });
+                    }, 2000);
+    
+    
                 }
             });
+    
+            $(document).on( 'pagebeforeshow', '#machines', function() {
+                $('#machines-list').listview('refresh');
+            });
+    
+            $(document).on( 'pagebeforeshow', '#dialog-power', function() {
+                $("#dialog-power a").button();
+            });
+    
+            $(document).on( 'pagebeforeshow', '#dialog-single-power', function() {
+                $("#dialog-single-power a").button();
+            });
+    
+            $(document).on( 'pagebeforeshow', '#images', function() {
+                $("#images-list").listview('refresh');
+            });
+    
+            $(document).on( 'pagebeforeshow', '#single-machine', function() {
+                Mist.graphPolling = true;
+            });
+    
+            $(document).on( 'pageshow', '#single-machine', function() {
+                $(".monitoring-button").button();
+            });
+    
+            $(document).on( 'pagebeforehide', '#single-machine', function() {
+                Mist.graphPolling = false;
+            });
+    
+            // Console toggle behavior
+            $(document).ready(function() {
+                $('#shell-return').on('click', '.command', function() {
+                    var out = $(this).next('.output');
+                    if (out.is(':visible')) {
+                        out.slideUp(300);
+                        $(this).parent().addClass('contracted').removeClass('expanded');
+                    } else {
+                        out.slideDown(200);
+                        $(this).parent().removeClass('contracted').addClass('expanded');
+                    }
+                });
+            });
+    
+            App.Select = Ember.Select.extend({
+                attributeBindings: [
+                    'name',
+                    'data-theme',
+                    'data-icon',
+                    'data-native-menu',
+                    'disabled'
+                ],
+            });
+    
+            App.TextField = Ember.TextField.extend({
+                attributeBindings: [
+                    'name',
+                    'data-theme'
+                ]
+            });
+    
+            App.ShellTextField = Ember.TextField.extend({
+                attributeBindings: [
+                    'name',
+                    'data-theme',
+                    'autocapitalize'
+                ],
+    
+                insertNewline: function() {
+                    this.controller.submit();
+                }
+            });
+    
+            App.Checkbox = Ember.Checkbox.extend({
+                attributeBindings: [
+                    'name',
+                    'id',
+                    'data-inline'
+                ],
+            });
+    
+            App.CountView = Count;
+            App.BackendButtonView = BackendButton;
+            App.AddBackendView = AddBackend;
+            App.EditBackendView = EditBackend;
+            App.MachineListView = MachineList;
+            App.ImageListView = ImageList;
+            App.EnableBackendButtonView = EnableBackendButton;
+            App.DeleteTagView = DeleteTagView;
+            App.onOff = ['on', 'off'];
+    
+            var addDialog = MachineAddDialog.create();
+            addDialog.append();
+    
+            var machineView = MachineView.create();
+            machineView.append();
+    
+            var confirmationDialog = ConfirmationDialog.create();
+            confirmationDialog.append();
+    
+            var machineActionsDialog = MachineActionsDialog.create();
+            machineActionsDialog.append();
+            var dialog = SingleMachineActionsDialog.create();
+            dialog.append();
+            var shellDialog = Shell.create();
+            shellDialog.append();
+            var machineListView = MachineListView.create();
+            machineListView.append();
+            var imageListView = ImageListView.create();
+            imageListView.append();
+            var machineTagsDialog = MachineTagsDialog.create();
+            machineTagsDialog.append();
+            var machineMonitoringDialog = MachineMonitoringDialog.create();
+            machineMonitoringDialog.append();
+    
+            $(document).on( 'pagebeforeshow', '#dialog-add', function(){
+                $('#dialog-add').trigger('create');
+            });
+    
+            // Expose the application globally
+            return window.Mist = App;
         });
-
-        App.Select = Ember.Select.extend({
-            attributeBindings: [
-                'name',
-                'data-theme',
-                'data-icon',
-                'data-native-menu',
-                'disabled'
-            ],
-        });
-
-        App.TextField = Ember.TextField.extend({
-            attributeBindings: [
-                'name',
-                'data-theme'
-            ]
-        });
-
-        App.ShellTextField = Ember.TextField.extend({
-            attributeBindings: [
-                'name',
-                'data-theme',
-                'autocapitalize'
-            ],
-
-            insertNewline: function() {
-                this.controller.submit();
-            }
-        });
-
-        App.Checkbox = Ember.Checkbox.extend({
-            attributeBindings: [
-                'name',
-                'id',
-                'data-inline'
-            ],
-        });
-
-        App.CountView = Count;
-        App.BackendButtonView = BackendButton;
-        App.AddBackendView = AddBackend;
-        App.EditBackendView = EditBackend;
-        App.MachineListView = MachineList;
-        App.ImageListView = ImageList;
-        App.EnableBackendButtonView = EnableBackendButton;
-        App.DeleteTagView = DeleteTagView;
-        App.onOff = ['on', 'off'];
-
-        var addDialog = MachineAddDialog.create();
-        addDialog.append();
-
-        var machineView = MachineView.create();
-        machineView.append();
-
-        var confirmationDialog = ConfirmationDialog.create();
-        confirmationDialog.append();
-
-        var machineActionsDialog = MachineActionsDialog.create();
-        machineActionsDialog.append();
-        var dialog = SingleMachineActionsDialog.create();
-        dialog.append();
-        var shellDialog = Shell.create();
-        shellDialog.append();
-        var machineListView = MachineListView.create();
-        machineListView.append();
-        var imageListView = ImageListView.create();
-        imageListView.append();
-        var machineTagsDialog = MachineTagsDialog.create();
-        machineTagsDialog.append();
-        var machineMonitoringDialog = MachineMonitoringDialog.create();
-        machineMonitoringDialog.append();
-
-        $(document).on( 'pagebeforeshow', '#dialog-add', function(){
-            $('#dialog-add').trigger('create');
-        });
-
-        // Expose the application globally
-        return window.Mist = App;
     }
 );
 
