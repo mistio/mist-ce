dac40402ade448efe6008196fac316cdda888b75
Mike Muzurakis <clopy@unweb.me>
2013-03-08 18:52:06 +0200

    Notify user on key deletion if it is still associated with a VM

--- src/mist/io/static/js/app/views/key.js
+++ src/mist/io/static/js/app/views/key.js
@@ -15,11 +15,28 @@ define('app/views/key', [
 
             deleteKey: function() {
                 var key = this.key;
+                if (key.machines) {
+                    machineNames = [];
+                    key.machines.forEach(function(item){
+                        Mist.backendsController.content.forEach(function(backend){
+                            if (backend.id == item[0]) {
+                                backend.machines.content.forEach(function(machine){
+                                    if (machine.id == item[1]) {
+                                        machineNames.push(machine.name);
+                                    }
+                                });
+                            }
+                        });
+                    });
+                }
 
                 Mist.confirmationController.set('title', 'Delete Key: ' + key.name);
-                Mist.confirmationController.set('text', 'Are you sure you want to delete ' +
+                if (key.machines.length > 0) {
+                Mist.confirmationController.set('text', 'Your key is associated with ' + machineNames.toString() +'. Are you sure you want to delete ' +  key.name + '? You will not be able use console and monitoring on these VMs.');                    
+                } else {
+                    Mist.confirmationController.set('text', 'Are you sure you want to delete ' +
                                                 key.name + '?');
-
+                }
                 Mist.confirmationController.set('callback', function() {
                     key.deleteKey();
                     $.mobile.changePage('#keys');
