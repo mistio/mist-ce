f04a5aa0d5fb711b275fdf0f62ab035092fbd072
Christos Psaltis <cpsaltis@unweb.me>
2013-03-13 16:44:18 +0200

    Delete input text field showing private key

--- src/mist/io/static/js/app/views/key.js
+++ src/mist/io/static/js/app/views/key.js
@@ -79,7 +79,7 @@ define('app/views/key', [
 
                 Mist.confirmationController.set('title', 'Delete Key: ' + key.name);
                 if (key.machines.length > 0) {
-                Mist.confirmationController.set('text', 'Your key is associated with ' + machineNames.toString() +'. Are you sure you want to delete ' +  key.name + '? You will not be able use console and monitoring on these VMs.');                    
+                Mist.confirmationController.set('text', 'Your key is associated with ' + machineNames.toString() +'. Are you sure you want to delete ' +  key.name + '? You will not be able use console and monitoring on these VMs.');
                 } else {
                     Mist.confirmationController.set('text', 'Are you sure you want to delete ' +
                                                 key.name + '?');
@@ -114,10 +114,10 @@ define('app/views/key', [
                 this._super();
                 // cannot have template in home.pt as pt complains
                 this.set('template', Ember.Handlebars.compile(key_html));
-                $('.public-key input, .private-key input').live('click', function(){
+                $('.public-key input').live('click', function(){
                     this.select();
                 });
-                $('.public-key input, .private-key input').live('change', function(){
+                $('.public-key input').live('change', function(){
                    return false;
                 });
             },
