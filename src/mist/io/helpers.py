fce35c164e4e01ac3fac76dabb8d53a3813d05f0
Dimitris Moraitis <dimo@unweb.me>
2013-04-08 17:31:28 +0300

    move b58_encode outside of generate_backend_id

--- src/mist/io/helpers.py
+++ src/mist/io/helpers.py
@@ -443,20 +443,34 @@ def run_command(conn, machine_id, host, ssh_user, private_key, command):
 
 
 def generate_backend_id(provider, region, apikey):
-    alphabet = '123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ'
-    base_count = len(alphabet)     
-    def encode(num):
-        """ Returns num in a base58-encoded string """
-        encode = ''       
-        if (num < 0):
-            return ''
-        while (num >= base_count):    
-            mod = num % base_count
-            encode = alphabet[mod] + encode
-            num = num / base_count
-        if (num):
-            encode = alphabet[num] + encode
-        return encode
-    
     i = int(sha1('%s%s%s' % (provider, region, apikey)).hexdigest(), 16)
-    return encode(i)
+    return b58_encode(i)
+
+
+alphabet = '123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ'
+base_count = len(alphabet)
+
+def b58_encode(num):
+    """ Returns num in a base58-encoded string """
+    encode = ''       
+    if (num < 0):
+        return ''
+    while (num >= base_count):    
+        mod = num % base_count
+        encode = alphabet[mod] + encode
+        num = num / base_count
+    if (num):
+        encode = alphabet[num] + encode
+    return encode
+
+
+def b58_decode(s):
+    """ Decodes the base58-encoded string s into an integer """
+    decoded = 0
+    multi = 1
+    s = s[::-1]
+    for char in s:
+        decoded += multi * alphabet.index(char)
+        multi = multi * base_count
+        
+    return decoded
