fdbcdf7f0910d2379dc481d598ad41433da48610
Christos Psaltis <cpsaltis@unweb.me>
2013-04-08 17:42:32 +0300

    Clean up after changes in multiple selection and create wizard

--- src/mist/io/static/js/app/controllers/select_machines.js
+++ src/mist/io/static/js/app/controllers/select_machines.js
@@ -8,11 +8,10 @@ define('app/controllers/select_machines', [
      */
     function() {
         return Ember.ArrayController.extend({
+
             _content: [{name: 'All', id: 'all'},
                       { name: 'None', id: 'none'}],
-            selection: null,
 
-            //if only this would work...
             backendsChanged: function(){
                 log("backends changed");
                 var content = new Array();
@@ -25,52 +24,8 @@ define('app/controllers/select_machines', [
                     content.push({name: item.title, id: item.provider});
                 });
 
-                //this.contentWillChange();
                 this.set('content', content);
-                //this.contentDidChange();
-            }.observes('Mist.backendsController.@each'),
-
-            selectionChanged: function(){
-
-                var selection = this.selection.id;
-
-                if(selection == 'none'){
-                    Mist.backendsController.forEach(function(backend){
-                        backend.machines.forEach(function(machine){
-                            log('deselecting machine: ' + machine.name);
-                            machine.set('selected', false);
-                        });
-                    });
-                } else if(selection == 'all'){
-                    Mist.backendsController.forEach(function(backend){
-                        backend.machines.forEach(function(machine){
-                            log('selecting machine: ' + machine.name);
-                            machine.set('selected', true);
-                        });
-                    });
-                } else {
-                    Mist.backendsController.forEach(function(backend){
-                        if(backend.provider == selection){
-                            backend.machines.forEach(function(machine){
-                                log('selecting machine: ' + machine.name);
-                                machine.set('selected', true);
-                            });
-                        } else {
-                            backend.machines.forEach(function(machine){
-                                log('deselecting machine: ' + machine.name);
-                                machine.set('selected', false);
-                            });
-                        }
-                    });
-                }
-                Ember.run.next(function(){
-                    $("input[type='checkbox']").checkboxradio("refresh");
-                });
-            }.observes('selection'),
-
-            init: function() {
-                this._super();
-            }
+            }.observes('Mist.backendsController.@each')
         });
     }
 );
