49880b13c0237018d1a5bb5fe72cb144280f6d64
Markos Gogoulos <mgogoulos@unweb.me>
2013-06-03 05:57:20 -0700

    get loadavg stats when machine is monitored

--- src/mist/io/static/js/app/views/machine_list_item.js
+++ src/mist/io/static/js/app/views/machine_list_item.js
@@ -1,7 +1,3 @@
-                    //setTimeout(function(){
-
-                    //},10);
-
 define('app/views/machine_list_item', [
     'text!app/templates/machine_list_item.html','ember'],
     /**
@@ -14,27 +10,36 @@ define('app/views/machine_list_item', [
         return Ember.View.extend({
                 tagName:false,
 
+                fetchLoadavg: function(machine){
+                    if (machine.hasMonitoring){
+                        var uri = URL_PREFIX + '/backends/' + machine.backend.id + '/machines/' + machine.id + '/loadavg.png';
+                        var bgimage = new Image();
+                        var timestamp = new Date().getTime();
+                        bgimage.src = uri + '?' + timestamp;
+                        // update load graph after the image is loaded
+                        bgimage.onload = function () {
+                           $('#' + machine.id + ' span.monitoring-icon').css('background-image', 'url(' + bgimage.src + ')');
+                        };
+                        timestamp = new Date().getTime();
+                    }
+                },
+
                 didInsertElement: function(){
                     $('#machines-list').listview('refresh');
                     $("#machines-list").trigger('create');
-
-                    var machine = this.machine;                
-                    var uri = URL_PREFIX + '/backends/' + machine.backend.id + '/machines/' + machine.id + '/loadavg.png';
-                    setInterval(function(){
-                        warn('1');
-                        if (machine.hasMonitoring){
-                            var bgimage = new Image();
-                            var timestamp = new Date().getTime();
-                            bgimage.src = uri + '?' + timestamp;
-                            // update load graph after the image is loaded
-                            bgimage.onload = function () {
-                               $('#' + machine.id + ' span.monitoring-icon').css('background-image', 'url(' + bgimage.src + ')');
-                            };
-                            timestamp = new Date().getTime();
-                        }
-                    }, 3*60*1000);
-
+                    var machine = this.machine;
+                    setInterval(this.fetchLoadavg, 4*60*1000, machine);
                 },
+
+                disabledMonitoringClass: function() {
+                    var machine = this.machine;
+                    if (machine && machine.hasMonitoring) {
+                        this.fetchLoadavg(machine);
+                        return '';
+                    } else {
+                        return 'ui-disabled';
+                    }
+                }.property('machine.hasMonitoring'),
 
                 machineSelected: function(){
                     var len = 0;
