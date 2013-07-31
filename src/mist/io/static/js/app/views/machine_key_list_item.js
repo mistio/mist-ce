749a9b76febfc95de3eef95de4dcb99249b442bb
Mike Muzurakis <clopy@unweb.me>
2013-07-31 12:39:55 +0300

    ability to associate a user name with a machine/key

--- src/mist/io/static/js/app/views/machine_key_list_item.js
+++ src/mist/io/static/js/app/views/machine_key_list_item.js
@@ -18,6 +18,29 @@ define('app/views/machine_key_list_item', [
                 $('.' + this.key.name + ' .ajax-loader').show();
                 var machine = this.get('controller').get('model');
                 Mist.keysController.disassociateKey(this.key, machine);
+            },
+
+            getKeyUser: function() {
+                var machine = this.get('controller').get('model');
+                var keyUser = 'root';
+                this.key.machines.forEach(function(machineKey) {
+                    if (machineKey[1] == machine.id) {
+                        if (machineKey[2]) {
+                            keyUser = machineKey[2];
+                        } else {
+                            keyUser = machine.getUser();
+                        }
+                    }
+                });
+                return keyUser;
+            }.property('this.getKeyUser'),
+
+            updateKeyUser: function(event) {
+                $('.' + this.key.name + ' .delete-key-container').hide();
+                $('.' + this.key.name + ' .ajax-loader').show();
+                var machine = this.get('controller').get('model');
+                var keyUser = $('.' + this.key.name + ' input').val();
+                Mist.keysController.associateUserKey(keyUser, this.key.name, machine);
             }
         });
     }
