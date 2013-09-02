0dec43a8dfbcacf7c93f4f8559e03a3a7348b85d
Clopy <clopy@unweb.me>
2013-09-02 14:02:39 +0300

    fix corners on trial button

--- src/mist/io/static/js/app/models/machine.js
+++ src/mist/io/static/js/app/models/machine.js
@@ -439,8 +439,9 @@ define('app/models/machine', [
                             }
                             $('#enable-monitoring-dialog').hide();
                             $('#monitoring-enabled').hide();
-                            $('#plan-dialog').show(); 
+                            $('#plan-dialog').show();
                             $('#plan-text span').text('Monitoring is a premium service. You can try it for free for one month, or purchase a plan.');
+                            $('#button-enable-trial').parent().addClass('ui-last-child');
                             $('#button-enable-trial').closest('.ui-btn').show();
                             $('#button-purchase').closest('.ui-btn').hide();
                         }
