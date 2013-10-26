5d9fd259a3beed159e6d76577eb922c5a4a45fdb
George Tsopanoglou <youtsop@gmail.com>
2013-10-26 16:21:24 +0300

    Fix home page colors and a jqm bug for controlgroups

--- src/mist/io/static/js/app/views/backend_button.js
+++ src/mist/io/static/js/app/views/backend_button.js
@@ -15,6 +15,12 @@ define('app/views/backend_button', ['ember'],
             didInsertElement: function() {
                 if ('button' in $("#"+this.elementId)) {
                     $("#"+this.elementId).button();
+                    /*
+                     *  WARNING: The following line of code is a quick fix 
+                     *  for a bug caused by jqm-1.4.0.rc.1.js 
+                     *  TODO: Remove code when bug get's fixed by jqm
+                     */
+                    $("#"+this.elementId).removeClass('ui-link ui-btn ui-icon-check ui-btn-icon-left ui-shadow ui-corner-all');
                 }
                 if ('controlgroup' in $('#backend-buttons')) {
                     $('#backend-buttons').controlgroup('refresh');
