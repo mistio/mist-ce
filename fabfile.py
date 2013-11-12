9e26f8a232f620581075a29aef6def362eac208c
Dimitris Rozakis <dimrozakis@gmail.com>
2013-11-12 15:24:15 +0200

    removed fabric completely

--- fabfile.py
+++
@@ -1,9 +0,0 @@
-from fabric.api import env
-
-env.command_timeout = 200
-env.abort_on_prompts = True
-env.no_keys = True
-env.no_agent = True
-env.warn_only = True
-env.combine_stderr = True
-env.keepalive = 15
