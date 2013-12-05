73e82ec5e29e9332e1fa602d716a706480095b1a
George Tsopanoglou <youtsop@gmail.com>
2013-12-05 21:15:35 +0200

    clean up machine list html

--- src/mist/io/static/js/app/views/home.js
+++ src/mist/io/static/js/app/views/home.js
@@ -22,8 +22,15 @@ define('app/views/home', ['app/views/mistscreen','text!app/templates/home.html',
              */
             
             whatCanIPossiblyDoChrome: function() {
+                // Desperate
                 Ember.run.later(function() {
+                    // ...
                     $('#backend-buttons').css('display', 'inline-block');
+                    Ember.run.next(function() {
+                        // At this point, I want to mention that the 
+                        // display attribute was set to block beforehand
+                        $('#backend-buttons').css('display', 'block');
+                    });
                 }, 100);
             }.on('didInsertElement'),
 
