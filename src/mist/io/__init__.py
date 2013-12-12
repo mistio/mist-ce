75a8b493abfb068f86cf1c58ecbf105adcd2e862
Dimitris Rozakis <dimrozakis@mist.io>
2013-12-12 18:54:59 +0200

    fix issues with io-core intercommunication
    log in at start up, save useful data temporarily in
    request.registry.session, send auth flag nicely to js

--- src/mist/io/__init__.py
+++ src/mist/io/__init__.py
@@ -12,7 +12,8 @@ from pyramid.config import Configurator
 
 
 from mist.io.resources import Root
-## from mist.io.model import User
+import mist.io.config
+from mist.io.model import User
 
 
 log = logging.getLogger(__name__)
@@ -23,59 +24,29 @@ def main(global_config, **settings):
     if not settings.keys():
         settings = global_config
 
-    # Import settings using sensible defaults where applicable
-    yaml_settings = os.getcwd() + "/settings.yaml"
-    try:
-        config_file = open(yaml_settings, 'r')
-    except IOError:
-        log.warn("settings.yaml does not exist.")
-        config_file = open(yaml_settings, 'w')
-        config_file.close()
-        config_file = open(yaml_settings, 'r')
-
-    try:
-        config = yaml.load(config_file) or {}
-    except:
-        log.error("Error parsing settings.yaml")
-        config = {}
-    finally:
-        config_file.close()
-
     settings = {}
-    settings['js_build'] = config.get('js_build',
-                                      settings.get('js_build', True))
-    settings['js_log_level'] = config.get('js_log_level', 3)
-    settings['default_poll_interval'] = config.get('default_poll_interval',
-                                                   10000)
-    settings['core_uri'] = config.get('core_uri', 'http://mist.io')
-    settings['google_analytics_id'] = config.get('google_analytics_id', '')
-
-## FIXME
-## # try to authenticate with mist.io service if email,password are available
-## user = User()   # this automatically loads from db.yaml
-## if user.email and user.password:
-    ## payload = {'email': user.email, 'password': user.password}
-    ## ret = requests.post(settings['core_uri'] + '/auth',
-                        ## params=payload, verify=False)
-##
-## #OLD
-## #if settings['user'].get('email') and settings['user'].get('password'):
-## #    payload = {'email': settings['user'].get('email'),
-## #               'password': settings['user'].get('password')}
-## #    ret = requests.post(settings['core_uri'] + '/auth', params=payload, verify=False)
-    ## if ret.status_code == 200:
-        ## settings['auth'] = 1
-        ## ret = json.loads(ret.content)
-        ## settings['current_plan'] = ret.get('current_plan',{})
-        ## settings['auth_key'] = ret.get('auth_key', '')
-        ## user_details = ret.get('user_details', {})
-        ## settings['user']['name'] = user_details.get('name', '')
-        ## settings['user']['company_name'] = user_details.get('company_name', '')
-        ## settings['user']['country'] = user_details.get('country', '')
-        ## settings['user']['number_of_servers'] = user_details.get('number_of_servers', '')
-        ## settings['user']['number_of_people'] = user_details.get('number_of_people', '')
-    ## else:
-        ## settings['auth'] = 0
+    # try to authenticate with mist.io service if email,password are available
+    user = User()   # this automatically loads from db.yaml
+    if user.email and user.password:
+        payload = {'email': user.email, 'password': user.password}
+        ret = requests.post(mist.io.config.CORE_URI + '/auth',
+                            params=payload, verify=False)
+        if ret.status_code == 200:
+            log.info("Succesfully authenticated to mist.io service.")
+            settings['auth'] = True
+            ret = json.loads(ret.content)
+            settings['current_plan'] = ret.get('current_plan', {})
+            settings['auth_key'] = ret.get('auth_key', '')
+            ## # FIXME: do we really need the following params?
+            ## user_details = ret.get('user_details', {})
+            ## settings['user']['name'] = user_details.get('name', '')
+            ## settings['user']['company_name'] = user_details.get('company_name', '')
+            ## settings['user']['country'] = user_details.get('country', '')
+            ## settings['user']['number_of_servers'] = user_details.get('number_of_servers', '')
+            ## settings['user']['number_of_people'] = user_details.get('number_of_people', '')
+        else:
+            log.error("Error authenticating to mist.io service. %d: %s", ret.status_code, ret.text)
+            settings['auth'] = False
 
     config = Configurator(root_factory=Root, settings=settings)
     config.add_static_view('resources', 'mist.io:static')
--- src/mist/io/views.py
+++ src/mist/io/views.py
@@ -92,11 +92,11 @@ def home(request):
         'email': user.email,
         'supported_providers': config.SUPPORTED_PROVIDERS,
         'core_uri': config.CORE_URI,
-        'auth': request.registry.settings.get('auth', 0),        #FIXME
+        'auth': request.registry.settings.get('auth') and 1 or 0,
         'js_build': config.JS_BUILD,
         'js_log_level': config.JS_LOG_LEVEL,
         'google_analytics_id': config.GOOGLE_ANALYTICS_ID,
-        'is_core': False
+        'is_core': 0
     }
 
 
