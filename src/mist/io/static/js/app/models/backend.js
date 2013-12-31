cd38c7a5716f387b2f16b91dc02cab9e516409a6
George Tsopanoglou <youtsop@gmail.com>
2013-12-31 15:58:17 +0200

    Run models/backend.js through jsbeautifier for test

--- src/mist/io/static/js/app/models/backend.js
+++ src/mist/io/static/js/app/models/backend.js
@@ -1,11 +1,12 @@
 define('app/models/backend', ['app/controllers/machines', 'app/controllers/images', 'app/controllers/sizes',
-                              'app/controllers/locations', 'ember'],
+        'app/controllers/locations', 'ember'
+    ],
     /**
      *  Backend Model
      *
      *  @returns Class
      */
-    function(MachinesController, ImagesController, SizesController, LocationsController) {
+    function (MachinesController, ImagesController, SizesController, LocationsController) {
         return Ember.Object.extend(Ember.Evented, {
 
             /**
@@ -27,52 +28,64 @@ define('app/models/backend', ['app/controllers/machines', 'app/controllers/image
             images: null,
             machines: null,
             locations: null,
-            
+
             sizeCount: 0,
             imageCount: 0,
             machineCount: 0,
             locationCoount: 0,
-            
+
             loadingSizes: null,
             loadingImages: null,
             loadingMachines: null,
             loadingLocations: null,
 
             /**
-             * 
+             *
              *  Initialization
-             * 
+             *
              */
 
-            load: function() {
-                Ember.run(this, function() {
+            load: function () {
+                Ember.run(this, function () {
                     // Add controllers
-                    this.sizes = SizesController.create({backend: this, content: []});
-                    this.images = ImagesController.create({backend: this, content: []});
-                    this.machines = MachinesController.create({backend: this, content: []});
-                    this.locations = LocationsController.create({backend: this, content: []});
-                    
+                    this.sizes = SizesController.create({
+                        backend: this,
+                        content: []
+                    });
+                    this.images = ImagesController.create({
+                        backend: this,
+                        content: []
+                    });
+                    this.machines = MachinesController.create({
+                        backend: this,
+                        content: []
+                    });
+                    this.locations = LocationsController.create({
+                        backend: this,
+                        content: []
+                    });
+
                     // Add events
                     this.sizes.on('onSizeListChange', this, '_updateSizeCount');
                     this.images.on('onImageListChange', this, '_updateImageCount');
                     this.machines.on('onMachineListChange', this, '_updateMachineCount');
                     this.locations.on('onLocationListChange', this, '_updateLocationCount');
                     this.machines.on('onSelectedMachinesChange', this, '_updateSelectedMachines');
-                    
+
                     // Add observers
-                    this.sizes.addObserver('loading', this, function() {
+                    this.sizes.addObserver('loading', this, function () {
                         Ember.run.once(this, 'loadingSizesObserver');
                     });
-                    this.images.addObserver('loading', this, function() {
+                    this.images.addObserver('loading', this, function () {
                         Ember.run.once(this, 'loadingImagesObserver');
                     });
-                    this.machines.addObserver('loading', this, function() {
+                    this.machines.addObserver('loading', this, function () {
                         Ember.run.once(this, 'loadingMachinesObserver');
                     });
-                    this.locations.addObserver('loading', this, function() {
+                    this.locations.addObserver('loading', this, function () {
                         Ember.run.once(this, 'loadingLocationsObserver');
                     });
-    
+
                     // Load data
                     this.sizes.load();
                     this.images.load();
@@ -84,112 +97,112 @@ define('app/models/backend', ['app/controllers/machines', 'app/controllers/image
 
 
             /**
-             * 
+             *
              *  Methods
-             * 
+             *
              */
 
-            getSize: function(sizeId) {
+            getSize: function (sizeId) {
                 return this.sizes.getSize(sizeId);
             },
 
 
-            getImage: function(imageId) {
+            getImage: function (imageId) {
                 return this.images.getImage(imageId);
             },
 
 
-            getMachine: function(machineId) {
+            getMachine: function (machineId) {
                 return this.machines.getMachine(machineId);
             },
 
 
-            getLocation: function(locationId) {
+            getLocation: function (locationId) {
                 return this.locations.getLocation(locationId);
             },
 
 
-            getMonitoredMachines: function() {
+            getMonitoredMachines: function () {
                 return this.machines.getMonitoredMachines();
             },
 
 
-            shutdownMachine: function(machineId) {
+            shutdownMachine: function (machineId) {
                 this.machines.shutdownMachine(machineId);
             },
 
 
-            destroyMachine: function(machineId) {
-               this.machines.destroyMachine(machineId);
+            destroyMachine: function (machineId) {
+                this.machines.destroyMachine(machineId);
             },
 
 
-            rebootMachine: function(machineId) {
-               this.machines.rebootMachine(machineId);
+            rebootMachine: function (machineId) {
+                this.machines.rebootMachine(machineId);
             },
 
 
-            startMachine: function(machineId) {
-               this.machines.startMachine(machineId);
+            startMachine: function (machineId) {
+                this.machines.startMachine(machineId);
             },
 
 
-            searchImages: function(filter, callback) {
+            searchImages: function (filter, callback) {
                 this.images.searchImages(filter, callback);
             },
 
 
-            toggleImageStar: function(imageId) {
+            toggleImageStar: function (imageId) {
                 this.images.toggleImageStar(imageId);
             },
 
 
             /**
-             * 
+             *
              *  Pseudo-Private Methods
-             * 
+             *
              */
 
-            _updateSizeCount: function() {
-                Ember.run(this, function() {
+            _updateSizeCount: function () {
+                Ember.run(this, function () {
                     this.set('sizeCount', this.sizes.content.length);
                     this.trigger('onSizeListChange');
                 });
             },
 
 
-            _updateImageCount: function() {
-                Ember.run(this, function() {
+            _updateImageCount: function () {
+                Ember.run(this, function () {
                     this.set('imageCount', this.images.content.length);
                     this.trigger('onImageListChange');
                 });
             },
 
 
-            _updateMachineCount: function() {
-                Ember.run(this, function() {
+            _updateMachineCount: function () {
+                Ember.run(this, function () {
                     this.set('machineCount', this.machines.content.length);
                     this.trigger('onMachineListChange');
                 });
             },
 
 
-            _updateLocationCount: function() {
-                Ember.run(this, function() {
+            _updateLocationCount: function () {
+                Ember.run(this, function () {
                     this.set('locationCount', this.locations.content.length);
                     this.trigger('onLocationListChange');
                 });
             },
 
 
-            _updateSelectedMachines: function() {
-                Ember.run(this, function() {
+            _updateSelectedMachines: function () {
+                Ember.run(this, function () {
                     this.set('selectedMachines', this.machines.selectedMachines);
                     this.trigger('onSelectedMachinesChange');
                 });
             },
 
-            _updateState: function() {
+            _updateState: function () {
                 if (this.enabled) {
                     if (this.loadingMachines || this.loadingImages || this.loadingSizes || this.loadingLocations) {
                         this.set('state', 'waiting');
@@ -204,12 +217,12 @@ define('app/models/backend', ['app/controllers/machines', 'app/controllers/image
 
 
             /**
-             * 
+             *
              *  Observers
-             * 
+             *
              */
 
-            enabledObserver: function() {
+            enabledObserver: function () {
                 if (this.enabled) {
                     this.sizes.load();
                     this.images.load();
@@ -226,32 +239,32 @@ define('app/models/backend', ['app/controllers/machines', 'app/controllers/image
 
 
             /**
-             * 
+             *
              *  Dynamic Observers
-             * 
+             *
              */
 
-            loadingSizesObserver: function() {
+            loadingSizesObserver: function () {
                 this.set('loadingSizes', this.sizes.loading);
             },
 
 
-            loadingImagesObserver: function() {
+            loadingImagesObserver: function () {
                 this.set('loadingImages', this.images.loading);
             },
 
 
-            loadingMachinesObserver: function() {
+            loadingMachinesObserver: function () {
                 this.set('loadingMachines', this.machines.loading);
             },
 
 
-            loadingLocationsObserver: function() {
+            loadingLocationsObserver: function () {
                 this.set('loadingLocations', this.locations.loading);
             },
 
 
-            stateObserver: function() {
+            stateObserver: function () {
                 Ember.run.once(this, '_updateState');
             }.observes('loadingMachines', 'loadingImages', 'loadingSizes', 'loadingLocations', 'enabled'),
         });
