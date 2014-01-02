f795f3f6cd4aeb36e81227102bff2b8f4e47068f
Markos Gogoulos <provetza@unweb.me>
2014-01-02 07:39:59 -0800

    Change collectd (un)deploy command.
    We now decouple our instance of collectd. It runs standalone from a custom dir
    without affecting the machine's collectd installation. We only use the binary if
    it already exists.

--- src/mist/io/static/js/app/models/machine.js
+++ src/mist/io/static/js/app/models/machine.js
@@ -373,14 +373,14 @@ define('app/models/machine', [
                             if (prefix.slice(-1) == '/') {
                                 prefix = prefix.substring(0, prefix.length - 1);
                             }
-                            var cmd = 'wget --no-check-certificate ' + prefix + '/core/scripts/deploy_collectd.sh -O - > /tmp/deploy_collectd.sh && $(command -v sudo) chmod +x /tmp/deploy_collectd.sh && $(command -v sudo) /tmp/deploy_collectd.sh ' + data['monitor_server'] + ' ' + data['uuid'] + ' ' + data['passwd'];
+                            var cmd = '$(command -v sudo) mkdir -p /opt/mistio-collectd/ && $(command -v sudo) chmod go+rwX /opt/mistio-collectd && wget --no-check-certificate ' + prefix + '/core/scripts/deploy_collectd.sh -O - > /opt/mistio-collectd/deploy_collectd.sh && $(command -v sudo) chmod +x /opt/mistio-collectd/deploy_collectd.sh && $(command -v sudo) /opt/mistio-collectd/deploy_collectd.sh ' + data['monitor_server'] + ' ' + data['uuid'] + ' ' + data['passwd'];
                             //cmd = "sudo su -c '" + cmd + "' || " + cmd;
                             collectd_install_target = that;
                             warn(cmd);
                             that.shell(cmd, function(){}, timeout=300);
                         } else {
                             $('.pending-monitoring h1').text('Disabling collectd');
-                            var cmd = '$(command -v sudo) chmod -x /etc/init.d/collectd && $(command -v sudo) killall -9 collectd';
+                            var cmd = '$(command -v sudo) rm -f /etc/cron.d/mistio-collectd && $(command -v sudo) kill -9 `cat /opt/mistio-collectd/collectd.pid`';
                             //cmd = "sudo su -c '" + cmd + "' || " + cmd;
                             collectd_uninstall_target = that;
                             that.shell(cmd, function(){});
