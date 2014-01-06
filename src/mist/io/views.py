8a2df07cd08e3d94e777e6a70c7db10a1395513a
Dimitris Rozakis <dimrozakis@mist.io>
2014-01-06 21:01:08 +0200

    Import overriden view_config from core if it exists

--- src/mist/io/views.py
+++ src/mist/io/views.py
@@ -18,15 +18,15 @@ import requests
 import json
 
 from pyramid.response import Response
-from pyramid.view import view_config
-
 
 try:
     from mist.core import config
     from mist.core.helpers import user_from_request
+    from mist.core.helpers import view_config
 except ImportError:
     from mist.io import config
     from mist.io.helpers import user_from_request
+    from pyramid.view import view_config
 
 from mist.io import methods
 from mist.io.model import Keypair
@@ -249,7 +249,7 @@ def rename_backend(request):
     new_name = request.json_body.get('new_name', '')
     if not new_name:
         raise RequiredParameterMissingError('new_name')
-    
+
     user = user_from_request(request)
     methods.rename_backend(user, backend_id, new_name)
     return OK
