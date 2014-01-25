8ff9c9b3ae2f9b2d2782382e7229d02441b3b17a
George Tsopanoglou <youtsop@gmail.com>
2014-01-25 16:35:41 +0200

    Fix notification controller
    Default notification html was removed from the
    DOM on page change

--- src/mist/io/static/js/app/controllers/notification.js
+++ src/mist/io/static/js/app/controllers/notification.js
@@ -11,7 +11,8 @@ define('app/controllers/notification', ['ember'],
              *  Properties
              */
 
-            timeout: false,
+            timeout: null,
+
 
             /**
              *
@@ -20,41 +21,20 @@ define('app/controllers/notification', ['ember'],
              */
 
             notify: function (message) {
-                $.mobile.loading('show', {
-                    text: message,
-                    textVisible: true,
-                    textonly: true,
-                    theme: 'b'
-                });
+                $('#notification-popup h1').text(message);
+                $('#notification-popup').show();
                 Ember.run.later(function () {
-                    $.mobile.loading('hide');
+                    $('#notification-popup').hide();
                 }, this.timeout ? this.timeout : 2000);
             },
 
-            warn: function (message) {
-                $.mobile.loading('show', {
-                    text: message,
-                    textVisible: true,
-                    textonly: true,
-                    theme: 'b'
-                });
-                Ember.run.later(function () {
-                    $.mobile.loading('hide');
-                }, this.timeout ? this.timeout : 5000);
-            },
 
             timeNotify: function (message, miliseconds) {
-                if (this.timeout) {
-                    clearTimeout(this.timeout);
-                }
-                log('warning: ' + message);
-                $.mobile.loading('show', {
-                    text: message,
-                    textVisible: true,
-                    textonly: true,
-                    theme: $.mobile.pageLoadErrorMessageTheme
-                });
-                this.timeout = setTimeout("$.mobile.loading( 'hide' )", miliseconds);
+                $('#notification-popup h1').text(message);
+                $('#notification-popup').show();
+                Ember.run.later(function () {
+                    $('#notification-popup').hide();
+                }, miliseconds);
             },
 
             showMessagebox: function() {
