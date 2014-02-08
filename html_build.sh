165742a2082c6b3351957c09d497df247ad35f2c
George Tsopanoglou <youtsop@gmail.com>
2014-02-08 22:00:15 +0200

    Optimize html build script

--- html_build.sh
+++ html_build.sh
@@ -1,59 +1,25 @@
+#####################################################
+#	
+#	Compiles multiple handlebars templates into
+#   a single file for optimized performacne
 #
-# Create a temporary directory to store compiled templates
-#
-
-COMPILED_DIR="./compiled_tmp"
-rm -rf "$COMPILED_DIR"
-mkdir "$COMPILED_DIR"
-
-
-#
-# Compile templates
-#
+####################################################
 
 TEMPLATES_DIR="src/mist/io/static/js/app/templates"
+BUILD_PATH="src/mist/io/static/js/app/templates/build.js"
 
+# Wrap file into a define statement to access it in app.js
+echo "define('app/templates/build', ['ember'], function() {
+" > $BUILD_PATH
+
+# Compile html files and append them to build.js
 for f in $TEMPLATES_DIR"/"*.html
 do
     echo "Compiling: $f"
-    FILE_NAME=$(basename "$f")
-    FILE_NAME="${FILE_NAME%.*}"
-    ember-precompile "$f" -f "$COMPILED_DIR""/""$FILE_NAME"".js"
+    ember-precompile "$f" >> $BUILD_PATH
 
 done
 
+# Close wrapper
+echo "});" >> $BUILD_PATH
 
-#
-# Concatinate files
-#
-
-echo""
-echo "Concatinating files..."
-HTML_BUILD="src/mist/io/static/js/app/templates/build.js"
-
-if [ -f $HTML_BUILD ]
-then 
-    rm $HTML_BUILD
-fi
-touch $HTML_BUILD
-
-for f in $COMPILED_DIR"/"*.js
-do
-    cat "$f" >> $HTML_BUILD
-done
-
-
-#
-# Add define header in build.js
-#
-
-echo "Adding define wrapper on: $HTML_BUILD"
-HEADER="define('app/templates/build', ['ember'], function() {
-"
-echo "$HEADER" | cat - "$HTML_BUILD" > temp && mv temp "$HTML_BUILD"
-echo "});" >> "$HTML_BUILD"
-
-#clean up
-echo "Cleaning up"
-rm -rf $COMPILED_DIR
-echo "Done"
