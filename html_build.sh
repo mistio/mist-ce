5774a3c1efc77c3fbd4ff2d66e99d8de2ccccb4c
George Tsopanoglou <youtsop@gmail.com>
2014-02-08 23:17:22 +0200

    Add progressbar in html_build script

--- html_build.sh
+++ html_build.sh
@@ -5,21 +5,42 @@
 #
 ####################################################
 
-TEMPLATES_DIR="src/mist/io/static/js/app/templates"
 BUILD_PATH="src/mist/io/static/js/app/templates/build.js"
+TEMPLATES_DIR="src/mist/io/static/js/app/templates"
+FILE_COUNT=`eval ls -l $TEMPLATES_DIR | grep .html | wc -l | tr -d ' '`
 
 # Wrap file into a define statement to access it in app.js
 echo "define('app/templates/build', ['ember'], function() {
 " > $BUILD_PATH
 
+
+# Prints a progress bar with value passed as first argument
+function progress {
+
+	value=`bc <<< "$1 / 1"`
+	half_value=`bc <<< "$value / 2"`
+	bar="|"
+	for (( int=1; int <= $half_value; ++int)); do bar="$bar="; done
+	for (( int=$half_value; int < 50; ++int)); do bar="$bar "; done
+    echo -ne "$bar| $value %\r"
+}
+
+echo "Compiling..."
+progress 0
+
 # Compile html files and append them to build.js
+i=0
 for f in $TEMPLATES_DIR"/"*.html
 do
-    echo "Compiling: $f"
     ember-precompile "$f" >> $BUILD_PATH
-
+    
+    # Show progressbar
+    i=$((i + 1))
+    progress `bc -l <<< "100 / $FILE_COUNT * $i"`
 done
 
 # Close wrapper
 echo "});" >> $BUILD_PATH
 
+progress 100
+echo -ne "\nDone\n"
