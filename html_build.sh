81bf41f59d84acf63f6d997ffebd8b468eb3f3bf
George Tsopanoglou <youtsop@gmail.com>
2014-02-08 23:26:20 +0200

    Finalize html_build script

--- html_build.sh
+++ html_build.sh
@@ -1,13 +1,17 @@
-#####################################################
+################################################################
 #	
 #	Compiles multiple handlebars templates into
 #   a single file for optimized performacne
 #
-####################################################
+#   Needs ember-precompile:
+#   https://github.com/gabrielgrant/node-ember-precompile
+#  
+################################################################
 
 BUILD_PATH="src/mist/io/static/js/app/templates/build.js"
 TEMPLATES_DIR="src/mist/io/static/js/app/templates"
 FILE_COUNT=`eval ls -l $TEMPLATES_DIR | grep .html | wc -l | tr -d ' '`
+
 
 # Wrap file into a define statement to access it in app.js
 echo "define('app/templates/build', ['ember'], function() {
@@ -20,13 +24,15 @@ function progress {
 	value=`bc <<< "$1 / 1"`
 	half_value=`bc <<< "$value / 2"`
 	bar="|"
-	for (( int=1; int <= $half_value; ++int)); do bar="$bar="; done
-	for (( int=$half_value; int < 50; ++int)); do bar="$bar "; done
+	for (( i=0; i <= $half_value; ++i)); do bar="$bar="; done
+	for (( i=$half_value; i < 50; ++i)); do bar="$bar "; done
     echo -ne "$bar| $value %\r"
 }
 
+
 echo "Compiling..."
 progress 0
+
 
 # Compile html files and append them to build.js
 i=0
@@ -39,8 +45,10 @@ do
     progress `bc -l <<< "100 / $FILE_COUNT * $i"`
 done
 
+
 # Close wrapper
 echo "});" >> $BUILD_PATH
+
 
 progress 100
 echo -ne "\nDone\n"
