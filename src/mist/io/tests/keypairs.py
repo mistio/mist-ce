c83641e6ee94f0b6fee2f42b6331aadf2aafe4c3
commixon <commixon@gmail.com>
2014-03-12 16:09:49 +0200

    Add csrf parameter in keypairs requests

--- src/mist/io/tests/keypairs.py
+++ src/mist/io/tests/keypairs.py
@@ -2,12 +2,13 @@ from MyRequestsClass import MyRequests
 import json
 
 
-def add_key(uri, name, private, cookie=None):
+def add_key(uri, name, private, cookie=None, csrf=None):
     payload = {
         'id': name,
         'priv': private
     }
-    req = MyRequests(uri=uri+"/keys", cookie=cookie, data=json.dumps(payload))
+    req = MyRequests(uri=uri+"/keys", cookie=cookie, data=json.dumps(payload),
+                     csrf=csrf)
     response = req.put()
     assert response.ok, u'\nGot %d Response Status: %s \n%s' % (response.status_code, response.reason, response.text)
 
@@ -18,17 +19,18 @@ def add_key(uri, name, private, cookie=None):
         assert False, u'Exception: %s' %e
 
 
-def edit_key(uri, key_id, new_name, cookie=None):
+def edit_key(uri, key_id, new_name, cookie=None, csrf=None):
     payload = {
         'new_id': new_name
     }
-    req = MyRequests(uri=uri+"/keys/" + key_id,  data=json.dumps(payload), cookie=cookie)
+    req = MyRequests(uri=uri+"/keys/" + key_id,  data=json.dumps(payload),
+                     cookie=cookie, csrf=csrf)
     response = req.put()
     assert response.ok, u'\nGot %d Response Status: %s \n%s' % (response.status_code, response.reason, response.text)
 
 
-def list_keys(uri, cookie=None):
-    req = MyRequests(uri=uri+"/keys", cookie=cookie)
+def list_keys(uri, cookie=None, csrf=None):
+    req = MyRequests(uri=uri+"/keys", cookie=cookie, csrf=csrf)
     response = req.get()
     assert response.ok, u'\nGot %d Response Status: %s \n%s' % (response.status_code, response.reason, response.text)
 
@@ -39,8 +41,8 @@ def list_keys(uri, cookie=None):
         assert False, u'Exception: %s' %e
 
 
-def delete_key(uri, key_id, cookie=None):
-    req = MyRequests(uri=uri+"/keys/"+key_id, cookie=cookie)
+def delete_key(uri, key_id, cookie=None, csrf=None):
+    req = MyRequests(uri=uri+"/keys/"+key_id, cookie=cookie, csrf=csrf)
     response = req.delete()
     assert response.ok, u'\nGot %d Response Status: %s \n%s' % (response.status_code, response.reason, response.text)
 
@@ -52,8 +54,8 @@ def delete_key(uri, key_id, cookie=None):
         assert False, u'Exception: %s' %e
 
 
-def generate_keypair(uri, cookie=None):
-    req = MyRequests(uri=uri+"/keys", cookie=cookie)
+def generate_keypair(uri, cookie=None, csrf=None):
+    req = MyRequests(uri=uri+"/keys", cookie=cookie, csrf=csrf)
     response = req.post()
     assert response.ok, u'\nGot %d Response Status: %s \n%s' % (response.status_code, response.reason, response.text)
 
@@ -65,8 +67,9 @@ def generate_keypair(uri, cookie=None):
         assert False, u'Exception: %s' %e
 
 
-def get_private_key(uri, key_id, cookie=None):
-    req = MyRequests(uri=uri+"/keys/"+key_id+"/private", cookie=cookie)
+def get_private_key(uri, key_id, cookie=None, csrf=None):
+    req = MyRequests(uri=uri+"/keys/"+key_id+"/private", cookie=cookie,
+                     csrf=csrf)
     response = req.get()
     assert response.ok, u'\nGot %d Response Status: %s \n%s' % (response.status_code, response.reason, response.text)
 
@@ -78,8 +81,9 @@ def get_private_key(uri, key_id, cookie=None):
         assert False, u'Exception: %s' %e
 
 
-def get_public_key(uri, key_id, cookie=None):
-    req = MyRequests(uri=uri+"/keys/"+key_id+"/public", cookie=cookie)
+def get_public_key(uri, key_id, cookie=None, csrf=None):
+    req = MyRequests(uri=uri+"/keys/"+key_id+"/public", cookie=cookie,
+                     csrf=csrf)
     response = req.get()
     assert response.ok, u'\nGot %d Response Status: %s \n%s' % (response.status_code, response.reason, response.text)
 
@@ -88,4 +92,4 @@ def get_public_key(uri, key_id, cookie=None):
         return params
     except Exception as e:
         print response.text
-        assert False, u'Exception: %s' %e
+        assert False, u'Exception: %s' % e
