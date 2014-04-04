832615859f8d1bdbeb4ccb59e11e523dfcf64dd7
George Tsopanoglou <youtsop@gmail.com>
2014-04-04 14:03:42 +0300

    Add controlled view

---
+++ src/mist/io/static/js/app/views/controlled.js
@@ -0,0 +1,49 @@
+define('app/views/controlled', ['app/views/templated'],
+    //
+    //  Controlled View
+    //
+    //  @returns Class
+    //
+    function (TemplatedView) {
+
+        'use strict';
+
+        return TemplatedView.extend({
+
+
+            //
+            //
+            //  Properties
+            //
+            //
+
+
+            controllerName: null,
+
+
+            //
+            //
+            //  Initialization
+            //
+            //
+
+
+            init: function () {
+                this._super();
+                this.set('controllerName', Mist.getViewName(this) + 'Controller');
+            },
+
+
+            didInsertElement: function () {
+                this._super();
+                Mist.get(this.controllerName).set('view', this);
+            },
+
+
+            willDestroyElement: function () {
+                this._super();
+                Mist.get(this.controllerName).set('view', null);
+            }
+        });
+    }
+);
