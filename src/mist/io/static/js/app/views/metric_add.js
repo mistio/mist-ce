629ce3a73013bd05f1460801d0f9f7e21f0a3a54
George Tsopanoglou <youtsop@gmail.com>
2014-06-10 17:08:28 +0300

    clean up code

--- src/mist/io/static/js/app/views/metric_add.js
+++ src/mist/io/static/js/app/views/metric_add.js
@@ -19,39 +19,6 @@ define('app/views/metric_add', ['app/views/popup'],
 
 
             clear: function () {
-                //$('#metric-add-metric').collapsible('option', 'collapsedIcon', 'arrow-d')
-                //             .collapsible('collapse');
-            },
-
-
-            //
-            //
-            //  Actions
-            //
-            //
-
-
-            actions: {
-
-                backClicked: function () {
-                    Mist.metricAddController.close();
-                },
-
-
-                addClicked: function () {
-                    Mist.metricAddController.add();
-                },
-
-
-                selectMetric: function (metric) {
-                    Mist.metricAddController.set('newMetric', {
-                        name: metric.name,
-                        newName: metric.name,
-                        target: metric.target
-                    });
-                    $('#metric-add-metric').collapsible('option', 'collapsedIcon', 'check')
-                                                 .collapsible('collapse');
-                }
             },
 
 
@@ -66,16 +33,7 @@ define('app/views/metric_add', ['app/views/popup'],
                 Ember.run.next(function () {
                     $('#metric-add-metric .ui-listview').listview('refresh');
                 });
-            }.observes('Mist.metricAddController.metrics'),
-
-
-            formReadyObserver: function () {
-                if (Mist.metricAddController.formReady ) {
-                    $('#metric-add-ok').removeClass('ui-state-disabled');
-                } else {
-                    $('#metric-add-ok').addClass('ui-state-disabled');
-                }
-            }.observes('Mist.metricAddController.formReady')
+            }.observes('Mist.metricAddController.metrics')
         });
     }
 );
