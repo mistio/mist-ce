171c10c245956552668eca473dcb347371728a73
Dimitris Rozakis <dimrozakis@mist.io>
2014-07-01 23:55:24 +0300

    Disable celery result backend

--- src/mist/io/celery_app.py
+++ src/mist/io/celery_app.py
@@ -15,16 +15,17 @@ except Exception as exc:
     log.error("Error parsing settings py: %r", exc)
 
 
-app = Celery('tasks',
-             backend=settings.get('CELERY_BACKEND', 'amqp://guest:guest@127.0.0.1//'),
-             broker=settings.get('CELERY_BROKER', 'amqp://guest:guest@127.0.0.1//'),
-             include=['mist.io.tasks'],
-            )
-
-app.conf.update(
-    CELERY_TASK_RESULT_EXPIRES=3600,
-    CELERYD_CONCURRENCY=16,
+app = Celery(
+    'tasks',
+    # backend=settings.get('CELERY_BACKEND', 'amqp://guest:guest@127.0.0.1//'),
+    broker=settings.get('CELERY_BROKER', 'amqp://guest:guest@127.0.0.1//'),
+    include=['mist.io.tasks'],
 )
+
+## app.conf.update(
+    ## CELERY_TASK_RESULT_EXPIRES=3600,
+    ## CELERYD_CONCURRENCY=16,
+## )
 
 if __name__ == '__main__':
     app.start()
