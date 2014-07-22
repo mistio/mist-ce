95528c47dae4b66c3d9c935fbbb8e316c3a25284
gtsop <gtsop.code@gmail.com>
2014-07-22 17:57:00 +0300

    Make "every" the default aggregate option

--- src/mist/io/static/js/app/models/rule.js
+++ src/mist/io/static/js/app/models/rule.js
@@ -43,7 +43,7 @@ define('app/models/rule', ['ember'],
             init: function () {
                 this._super();
                 // TODO: delete. This is temp for debugging only
-                this.set('aggregate', 'any');
+                this.set('aggregate', 'every');
                 this.set('timeWindow', 1);
             },
 
