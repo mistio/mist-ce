71e799053683c7791cfea965934e6d674d0035f7
gtsop <gtsop.code@gmail.com>
2014-08-10 02:35:19 +0300

    Refactor backend btn to make use of the TemplatedView so that we can use precompiled template

--- src/mist/io/static/js/app/views/backend_button.js
+++ src/mist/io/static/js/app/views/backend_button.js
@@ -1,80 +1,99 @@
-define('app/views/backend_button', ['ember'],
-    /**
-     *  Backend Button View
-     *
-     *  @returns Class
-     */
-    function() {
-        return Ember.View.extend({
+define('app/views/backend_button', ['app/views/templated'],
+    //
+    //  Backend Button View
+    //
+    //  @returns Class
+    //
+    function (TemplatedView) {
 
-            /**
-             *  Properties
-             */
+        'use strict';
 
-            tagName: 'a',
+        return TemplatedView.extend({
+
+
+            //
+            //
+            //  Properties
+            //
+            //
+
+
             backend: null,
-            attributeBindings: ['data-role', 'data-icon'],
-            template: Ember.Handlebars.compile('{{title}}'),
 
 
-            /**
-             *
-             *  Initialization
-             *
-             */
+            //
+            //
+            //  Initialization
+            //
+            //
 
-            renderButton: function() {
-                var btnElement = $('#'+this.elementId);
-                if (btnElement.button) {
-                    btnElement.button();
-                    if ($('#backend-buttons').controlgroup) {
-                        $('#backend-buttons').controlgroup('refresh');
-                    }
-                    this.stateObserver();
-                } else {
-                    Ember.run.later(this, function() {
-                        this.renderButton();
-                    }, 100);
-                }
+
+            load: function () {
+                this.renderBackends();
+                this.stateObserver();
             }.on('didInsertElement'),
 
 
-            destroyButton: function() {
-                Ember.run.next(function() {
-                    if ($('#backend-buttons').controlgroup) {
-                        $('#backend-buttons').controlgroup('refresh');
-                    }
-                });
+            unload: function() {
+                this.renderBackends();
             }.on('willDestroyElement'),
 
 
-            /**
-             *
-             *  Actions
-             *
-             */
+            //
+            //
+            //  Methods
+            //
+            //
 
-            click: function() {
-                $('#edit-backend-popup').popup('option', 'positionTo', '#' + this.elementId);
-                Mist.backendEditController.open(this.backend);
+
+            renderBackends: function () {
+                Ember.run.next(function() {
+                    if ($('#backend-buttons').controlgroup)
+                        $('#backend-buttons').controlgroup('refresh');
+                });
             },
 
 
-            /**
-             *
-             *  Observers
-             *
-             */
+            //
+            //
+            //  Actions
+            //
+            //
+
+
+            actions: {
+
+                buttonClicked: function() {
+                    $('#edit-backend-popup').popup('option',
+                        'positionTo', '#' + this.elementId);
+                    Mist.backendEditController.open(this.backend);
+                }
+            },
+
+
+            //
+            //
+            //  Observers
+            //
+            //
+
 
             stateObserver: function() {
-                $('#' + this.elementId).parent().removeClass('ui-icon-check ui-icon-offline ui-icon-waiting');
-                if (this.backend.state == 'online') {
-                    $('#' + this.elementId).parent().addClass('ui-icon-check');
-                } else if (this.backend.state == 'offline') {
-                    $('#' + this.elementId).parent().addClass('ui-icon-offline');
-                } else if (this.backend.state == 'waiting') {
-                    $('#' + this.elementId).parent().addClass('ui-icon-waiting');
-                }
+
+                var btn = $('#' + this.elementId + ' a');
+                btn.addClass('ui-btn-icon-left')
+                    .removeClass('ui-icon-check')
+                    .removeClass('ui-icon-offline')
+                    .removeClass('ui-icon-waiting');
+
+                if (this.backend.state == 'online')
+                    btn.addClass('ui-icon-check')
+                else if (this.backend.state == 'offline')
+                    btn.addClass('ui-icon-offline')
+                else if (this.backend.state == 'waiting')
+                    btn.addClass('ui-icon-waiting')
+                        .removeClass('ui-btn-icon-left');
+
             }.observes('backend.state')
         });
     }
