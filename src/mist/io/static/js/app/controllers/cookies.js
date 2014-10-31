897c5c90deb40e8c8c3c821efe4ff4e76a1f148b
gtsop <gtsop.code@gmail.com>
2014-10-31 22:08:23 +0200

    Replace cookie with localStorage

--- src/mist/io/static/js/app/controllers/cookies.js
+++ src/mist/io/static/js/app/controllers/cookies.js
@@ -166,22 +166,12 @@ define('app/controllers/cookies', ['ember'],
 
 
         function getCookie (cname) {
-            var name = cname + "=";
-            var ca = document.cookie.split(';');
-            for(var i=0; i<ca.length; i++) {
-                var c = ca[i];
-                while (c.charAt(0)==' ') c = c.substring(1);
-                if (c.indexOf(name) != -1) return c.substring(name.length,c.length);
-            }
-            return "";
+            return localStorage.getItem(cname);
         }
 
 
         function setCookie (cname, cvalue, exdays) {
-            var d = new Date();
-            d.setTime(d.getTime() + (exdays*24*60*60*1000));
-            var expires = "expires="+d.toUTCString();
-            document.cookie = cname + "=" + cvalue + "; " + expires;
+            localStorage.setItem(cname, cvalue);
             return getCookie(cname);
         }
 
