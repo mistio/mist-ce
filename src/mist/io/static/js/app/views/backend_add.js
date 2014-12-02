0972789b8a34fd48d50fec22bf21b8f63136bfa0
gtsop <gtsop.code@gmail.com>
2014-12-02 17:20:55 +0200

    Add vcloud support on add backend dialog

--- src/mist/io/static/js/app/views/backend_add.js
+++ src/mist/io/static/js/app/views/backend_add.js
@@ -47,6 +47,7 @@ define('app/views/backend_add', ['app/views/templated', 'ember'],
                 $('#docker-bundle').hide();
                 $('#baremetal-bundle').hide();
                 $('#openstack-bundle').hide();
+                $('#vcloud-bundle').hide();
                 $('#new-backend-provider').collapsible('collapse');
                 $('#new-backend-second-field').attr('type', 'password');
                 $('#gce-bundle a').removeClass('ui-icon-check')
@@ -81,6 +82,7 @@ define('app/views/backend_add', ['app/views/templated', 'ember'],
                     $('#docker-bundle').hide();
                     $('#hpcloud-bundle').hide();
                     $('#non-hp-cloud').hide();
+                    $('#vcloud-bundle').hide();
 
                     if (provider.provider.indexOf('rackspace') > -1 || provider.provider.indexOf('softlayer') > -1) {
                         this.set('firstFieldLabel', 'Username');
@@ -119,6 +121,11 @@ define('app/views/backend_add', ['app/views/templated', 'ember'],
                         this.set('secondFieldLabel', 'Password');
                         $('#opentack-advanced-wrapper').show();
                         $('#openstack-bundle').show();
+                    } else if (provider.provider.indexOf('vcloud') > -1) {
+                        this.set('firstFieldLabel', 'Username');
+                        this.set('secondFieldLabel', 'Password');
+                        $('#vcloud-bundle').show();
+                        Mist.backendAddController.set('newBackendHost', '');
                     } else if (provider.provider.indexOf('bare_metal') > -1) {
                         this.set('firstFieldLabel', 'Hostname');
                         this.set('secondFieldLabel', 'User');
