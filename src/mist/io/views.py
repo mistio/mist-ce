8373c8e57b1bb59504139de32f72ba37a038a579
gtsop <gtsop.code@gmail.com>
2014-12-15 17:47:30 +0200

    Return new provider dict

--- src/mist/io/views.py
+++ src/mist/io/views.py
@@ -85,7 +85,7 @@ def home(request):
     return {
         'project': 'mist.io',
         'email': json.dumps(user.email),
-        'supported_providers': json.dumps(config.SUPPORTED_PROVIDERS),
+        'supported_providers': json.dumps(config.SUPPORTED_PROVIDERS_V_2),
         'core_uri': json.dumps(config.CORE_URI),
         'auth': json.dumps(bool(user.mist_api_token)),
         'js_build': json.dumps(config.JS_BUILD),
