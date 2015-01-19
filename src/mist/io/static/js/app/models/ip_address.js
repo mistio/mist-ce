2b1ad9d49f96d974b1218768ffd8cfe223d6810a
gtsop <gtsop.code@gmail.com>
2015-01-19 19:10:11 +0200

    Add ip address model

---
+++ src/mist/io/static/js/app/models/ip_address.js
@@ -0,0 +1,70 @@
+define('app/models/ip_address', ['app/models/base'],
+    //
+    //  IP Address Model
+    //
+    //  @returns Class
+    //
+    function (BaseModel) {
+
+        'use strict';
+
+        return BaseModel.extend({
+
+
+            //
+            //
+            //  Properties
+            //
+            //
+
+
+            value: null,
+            server: null,
+            reserved: null,
+
+
+            //
+            //
+            //  Computed Properties
+            //
+            //
+
+
+            assignedServer: function () {
+
+            }.property('value', 'server')
+
+
+            isReserved: function () {
+
+            }.property('reserved'),
+
+
+            //
+            //
+            //  Methods
+            //
+            //
+
+
+            reserve: function () {
+
+            },
+
+
+            unreserve: function () {
+
+            },
+
+
+            assign: function (machine) {
+
+            },
+
+            
+            unassign: function () {
+
+            }
+        });
+    }
+);
