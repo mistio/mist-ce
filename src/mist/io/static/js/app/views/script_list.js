3fedbb8d59d4c199efe6b8344b2ec3ba5faa7d2a
gtsop <gtsop.code@gmail.com>
2015-02-19 14:54:04 +0200

    Fix log list view css and add script select/rename/delete in script list

--- src/mist/io/static/js/app/views/script_list.js
+++ src/mist/io/static/js/app/views/script_list.js
@@ -11,6 +11,40 @@ define('app/views/script_list', ['app/views/mistscreen'],
         return PageView.extend({
 
 
+            load: function () {
+
+                // Add event listeners
+                Mist.scriptsController.on('onSelectedChange', this, 'updateFooter');
+
+                this.updateFooter();
+
+            }.on('didInsertElement'),
+
+
+            unload: function () {
+
+                // Remove event listeners
+                Mist.scriptsController.off('onSelectedChange', this, 'updateFooter');
+
+            }.on('willDestroyElement'),
+
+            canRename: function () {
+                return Mist.scriptsController.get('selectedObjects').length == 1;
+            }.property('Mist.scriptsController.@each.selected'),
+
+            canDelete: function () {
+                return Mist.scriptsController.get('selectedObjects').length;
+            }.property('Mist.scriptsController.@each.selected'),
+
+            updateFooter: function () {
+                if (Mist.scriptsController.get('selectedObjects').length) {
+                    this.$('.ui-footer').slideDown();
+                }else {
+                    this.$('.ui-footer').slideUp();
+                }
+            },
+
+
             //
             //
             //  Actions
@@ -22,7 +56,48 @@ define('app/views/script_list', ['app/views/mistscreen'],
 
                 addClicked: function () {
                     Mist.scriptAddController.open();
-                }
+                },
+
+                renameClicked: function () {
+                    Mist.scriptEditController.open(Mist.scriptsController.get('selectedObjects')[0]);
+                },
+
+                selectClicked: function () {
+                    $('#select-scripts-popup').popup('open');
+                },
+
+                selectionModeClicked: function (mode) {
+                    $('#select-scripts-popup').popup('close');
+                    Ember.run(function () {
+                        Mist.scriptsController.forEach(function (script) {
+                            script.set('selected', mode);
+                        });
+                    });
+                },
+
+                deleteClicked: function () {
+
+                    var scripts = Mist.scriptsController.get('selectedObjects');
+
+                    Mist.dialogController.open({
+                        type: DIALOG_TYPES.YES_NO,
+                        head: 'Delete scripts',
+                        body: [
+                            {
+                                paragraph: 'Are you sure you want to delete these ' +
+                                    'scripts: ' + scripts.toStringByProperty('name') + ' ?'
+                            }
+                        ],
+                        callback: function (didConfirm) {
+                            if (!didConfirm) return;
+                            scripts.forEach(function (script) {
+                                Mist.scriptsController.deleteScript({
+                                    script: script
+                                });
+                            });
+                        }
+                    });
+                },
             }
         });
     }
--- src/mist/io/static/js/app/views/script_list_item.js
+++ src/mist/io/static/js/app/views/script_list_item.js
@@ -10,6 +10,19 @@ define('app/views/script_list_item', ['app/views/list_item'],
 
         return ListItemView.extend({
 
+            updateCheckbox: function () {
+                var element = this.$('input.ember-checkbox');
+                Ember.run.next(this, function () {
+                    if (element.checkboxradio) {
+                        element.checkboxradio()
+                               .checkboxradio('refresh');
+                    }
+                });
+            },
+
+            selectedObserver: function () {
+                Ember.run.once(this, 'updateCheckbox');
+            }.observes('script.selected')
         });
     }
 );
