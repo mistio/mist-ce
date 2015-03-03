b782c4c940266867cefc9d135136ecf9aa5bc991
Jenkins <info@mist.io>
2015-03-03 08:10:19 -0800

    Automated build of mist.js & mist.css

--- src/mist/io/static/js/app/templates/templates.js
+++ src/mist/io/static/js/app/templates/templates.js
@@ -2063,7 +2063,14 @@ function program33(depth0,data) {
     'placeholder': ("optional parameters"),
     'valueBinding': ("Mist.machineAddController.newMachineScriptParams")
   },hashTypes:{'id': "STRING",'data-theme': "STRING",'placeholder': "STRING",'valueBinding': "STRING"},hashContexts:{'id': depth0,'data-theme': depth0,'placeholder': depth0,'valueBinding': depth0},contexts:[depth0],types:["ID"],data:data})));
-  data.buffer.push("\n            </div>\n        </div>\n\n        <!-- Enable monitoring -->\n        <div id=\"create-machine-monitoring\" class=\"ui-state-disabled\">\n            <label>\n            ");
+  data.buffer.push("\n            </div>\n        </div>\n\n        <div id=\"ports\">\n            <!-- Expose Ports -->\n            <label>Ports:</label>\n            ");
+  data.buffer.push(escapeExpression(helpers.view.call(depth0, "Mist.TextArea", {hash:{
+    'id': ("create-machine-ports"),
+    'data-theme': ("a"),
+    'placeholder': ("e.g. 80:80"),
+    'valueBinding': ("Mist.machineAddController.newMachineDockerPorts")
+  },hashTypes:{'id': "STRING",'data-theme': "STRING",'placeholder': "STRING",'valueBinding': "STRING"},hashContexts:{'id': depth0,'data-theme': depth0,'placeholder': depth0,'valueBinding': depth0},contexts:[depth0],types:["ID"],data:data})));
+  data.buffer.push("\n        </div>\n\n        <!-- Enable monitoring -->\n        <div id=\"create-machine-monitoring\" class=\"ui-state-disabled\">\n            <label>\n            ");
   data.buffer.push(escapeExpression(helpers.view.call(depth0, "Mist.Checkbox", {hash:{
     'data-theme': ("a"),
     'checkedBinding': ("Mist.machineAddController.newMachineMonitoring")
