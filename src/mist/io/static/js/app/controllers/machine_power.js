87b397b5f58856b66673fb2f71b5d0bb0218701d
gtsop <gtsop.code@gmail.com>
2015-04-03 01:01:33 +0300

    Remove confirmation controller from machine power view

--- src/mist/io/static/js/app/controllers/machine_power.js
+++ src/mist/io/static/js/app/controllers/machine_power.js
@@ -46,18 +46,25 @@ define('app/controllers/machine_power', ['ember'],
                 // Close current popup
                 $('#machine-power-popup').popup('close');
 
-
                 var machineNames = this.machines.toStringByProperty('name');
 
-                // Show confirmation popup
                 var that = this;
                 Ember.run.later(function () {
-                    Mist.confirmationController.setUp('Machine ' + 'action',
-                        'Are you sure you want to ' + action + ' these machines: '
-                        + machineNames + ' ?', function () {
-                            that._act(action);
+                    Mist.dialogController.open({
+                        type: DIALOG_TYPES.YES_NO,
+                        head: 'Machine action',
+                        body: [
+                            {
+                                paragraph: 'Are you sure you want to ' + action + ' these machines: ' +
+                                    machineNames + ' ?'
+                            }
+                        ],
+                        callback: function (didConfirm) {
+                            if (didConfirm) {
+                                that._act(action);
+                            }
                         }
-                    );
+                    });
                 }, 500);
             },
 
