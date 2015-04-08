1b945106421b9b8de0a0557f9943e150e5f5cef5
commixon <commixon@gmail.com>
2015-04-08 20:03:14 +0300

    Add get_deploy_collectd_command_docker

--- src/mist/io/methods.py
+++ src/mist/io/methods.py
@@ -3702,6 +3702,10 @@ def get_deploy_collectd_command_windows(uuid, password, monitor):
      return '''powershell.exe -command "Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force;(New-Object System.Net.WebClient).DownloadFile('https://github.com/mistio/collectm/blob/build_issues/scripts/collectm.remote.install.ps1?raw=true', '.\collectm.remote.install.ps1');.\collectm.remote.install.ps1 -gitBranch ""build_issues"" -SetupConfigFile -setupArgs '-username """"%s"""""" -password """"""%s"""""" -servers @(""""""%s"""""") -interval 10'"''' % (uuid, password, monitor)
 
 
+def get_deploy_collectd_command_docker(uuid, password):
+    return "sudo docker run -d -v /sys/fs/cgroup:/sys/fs/cgroup -e COLLECTD_USERNAME=%s -e COLLECTD_PASSWORD=%s mist/collectd" % (uuid, password)
+
+
 def machine_name_validator(provider, name):
     """
     Validates machine names before creating a machine
