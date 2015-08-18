ac539b072e6a5228d761b7117e844b8172528b7f
Dimitris Moraitis <dimo@mist.io>
2015-08-18 23:36:23 +0300

    fix add cloud overlay

--- src/mist/io/static/js/app/views/page.js
+++ src/mist/io/static/js/app/views/page.js
@@ -26,13 +26,17 @@ define('app/views/page', ['app/views/templated'],
                         collapse: function( event ) {
                             $(this).children().next().slideUp(250);
                             var overlay=$('#' + $(this).attr('id')+'-overlay');
-                            if (overlay)
+                            if (overlay) {
                                 overlay.removeClass('in').addClass('ui-screen-hidden');
+                                $('body').css('overflow', 'auto');
+                            }
                         },
                         expand: function( event, ui ) {
                             var overlay=$('#' + $(this).attr('id')+'-overlay');
-                            if (overlay)
+                            if (overlay) {
                                 overlay.removeClass('ui-screen-hidden').addClass('in');
+                                $('body').css('overflow', 'hidden');
+                            }
                             $(this).children().next().hide();
                             $(this).children().next().slideDown(250);
                         }
