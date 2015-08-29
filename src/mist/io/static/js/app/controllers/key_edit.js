41b94e4f8c6d7b8d9744c10ee92a43bae89b33c4
Marios Fakiolas <fakiolasmarios@gmail.com>
2015-08-29 19:32:00 +0300

    machines power options / popups submit on enter key pressed

--- src/mist/io/static/js/app/controllers/key_edit.js
+++ src/mist/io/static/js/app/controllers/key_edit.js
@@ -24,7 +24,6 @@ define('app/controllers/key_edit', ['ember'],
              */
 
             open: function (keyId, callback) {
-                console.log(this.view.popupId);
                 this._clear();
                 this.setProperties({
                     keyId: keyId,
