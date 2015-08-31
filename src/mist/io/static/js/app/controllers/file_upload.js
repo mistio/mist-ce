464d9501e043fa0ef942da52004042f914d6bcf8
Marios Fakiolas <fakiolasmarios@gmail.com>
2015-08-31 08:08:45 +0300

    revert edit backend

--- src/mist/io/static/js/app/controllers/file_upload.js
+++ src/mist/io/static/js/app/controllers/file_upload.js
@@ -22,7 +22,7 @@ define('app/controllers/file_upload', ['ember'],
             title: null,
             label: null,
             callback: null,
-            uploadingFile: null,
+            uploadingFile: false,
 
 
             //
@@ -67,45 +67,52 @@ define('app/controllers/file_upload', ['ember'],
 
             uploadFile: function (args) {
 
+                this.set('uploadingFile', true);
+                return;
+
                 var that = this;
                 var reader = new FileReader();
                 var input = args.fileInput;
+
+
 
                 if (input.files && input.files[0]) {
                     var validExtensions = ['pem', 'txt', 'pub', ''],
                         filename = input.files[0].name,
 						ext = (filename.indexOf('.') > -1) ? filename.split('.').pop().toLowerCase() : '',
 						valid = validExtensions.indexOf(ext) > -1;
+                    console.log(1);
 
                     if (!valid) {
                         Mist.notificationController.notify('Please try to upload a valid file');
-                        console.log('Oh no, this is a .' + ext + ' file.');
-                        console.log('Marios lets me pass only .pem and .txt files!');
-                        return;
+                        that.set('uploadingFile', false);
+                        console.log(2);
+                    } else {
+                        console.log(3);
+                        reader.onloadend = function (e) {
+                            console.log(4);
+                            var success;
+                            if (e.target.readyState == FileReader.DONE) {
+                                that.set('file', e.target.result);
+                                success = true;
+                            } else {
+                                Mist.notificationController.notify('Failed to upload file');
+                                success = false
+                            }
+                            that.set('uploadingFile', false);
+                            if (args.fileInput)
+                                resetFileInputField($(args.fileInput));
+                            if (args.callback)
+                                args.callback(success, that.file);
+                        };
+                        console.log(5);
+                        this.set('uploadingFile', true);
+                        if (args.file)
+                            reader.readAsText(args.file, 'UTF-8');
+                        if (args.fileInput)
+                            reader.readAsText(args.fileInput.files[0], 'UTF-8');
                     }
                 }
-
-                reader.onloadend = function (e) {
-                    var success;
-                    if (e.target.readyState == FileReader.DONE) {
-                        that.set('file', e.target.result);
-                        success = true;
-                    } else {
-                        Mist.notificationController.notify('Failed to upload file');
-                        success = false
-                    }
-                    that.set('uploadingFile', false);
-                    if (args.fileInput)
-                        resetFileInputField($(args.fileInput));
-                    if (args.callback)
-                        args.callback(success, that.file);
-                };
-
-                this.set('uploadingFile', true);
-                if (args.file)
-                    reader.readAsText(args.file, 'UTF-8');
-                if (args.fileInput)
-                    reader.readAsText(args.fileInput.files[0], 'UTF-8');
             }
         });
     }
