8966be2cd28b83452bc501154af46c7e633eb991
ptzianos <pavlos.tzianos@gmail.com>
2015-10-20 23:49:11 +0300

    created a unified step for searching stuff and fixed the code that produces the random machine because suddenly it started throwing exceptions

--- src/mist/io/tests/gui/features/steps/general.py
+++ src/mist/io/tests/gui/features/steps/general.py
@@ -458,3 +458,18 @@ def go_to_some_page_without_waiting(context, title):
         except NoSuchElementException:
             pass
         sleep(1)
+
+
+@when(u'I search for a "{text}" {type_of_search}')
+def search_for_something(context, text, type_of_search):
+    type_of_search = type_of_search.lower()
+    if type_of_search not in ['machine', 'key', 'image', 'script', 'network']:
+        raise ValueError("This is type of object does not exist(%s)" % type_of_search)
+    search_bar = context.browser.find_elements_by_class_name("%s-search" % type_of_search)
+    assert len(search_bar) > 0, "Could not find the %s-search search input" % type_of_search
+    assert len(search_bar) == 1, "Found more than one %s-search search input " \
+                                 "elements" % type_of_search
+    search_bar = search_bar[0]
+    for letter in text:
+        search_bar.send_keys(letter)
+    sleep(2)
