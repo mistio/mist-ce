f1322505ce79150e0177b75edf86df00338a6da6
commixon <commixon@gmail.com>
2015-10-29 18:21:42 +0200

    Add missing param in async machine creation

--- src/mist/io/tasks.py
+++ src/mist/io/tasks.py
@@ -857,7 +857,7 @@ def create_machine_async(email, backend_id, key_id, machine_name, location_id,
                          quantity=1, persist=False, job_id=None,
                          docker_port_bindings={}, docker_exposed_ports={},
                          azure_port_bindings='', hostname='', plugins=None,
-                         associate_floating_ip=False):
+                         associate_floating_ip=False, associate_floating_ip_subnet=None):
     from multiprocessing.dummy import Pool as ThreadPool
     from mist.io.methods import create_machine
     from mist.io.exceptions import MachineCreationError
