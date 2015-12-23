bb9d4cf70a980a37a5dde9b8e2ab911c70a9b333
Dimitris Moraitis <dimo@mist.io>
2015-12-23 16:05:55 +0200

    remove Ember.run wrapper from _associateKey

--- src/mist/io/static/js/app/controllers/keys.js
+++ src/mist/io/static/js/app/controllers/keys.js
@@ -331,16 +331,14 @@ define('app/controllers/keys', ['app/models/key' , 'ember'],
 
 
             _associateKey: function(keyId, machine) {
-                Ember.run(this, function() {
-                    var key = this.getKey(keyId);
-                    key.machines.pushObject([machine.cloud.id, machine.id]);
-                    Mist.machineKeysController.probe(key);
-                    machine.setProperties({
-                        probed: true,
-                        keysCount: this.getMachineKeysCount(machine),
-                    });
-                    this.trigger('onKeyAssociate');
+                var key = this.getKey(keyId);
+                key.machines.pushObject([machine.cloud.id, machine.id]);
+                Mist.machineKeysController.probe(key);
+                machine.setProperties({
+                    probed: true,
+                    keysCount: this.getMachineKeysCount(machine),
                 });
+                this.trigger('onKeyAssociate');
             },
 
 
