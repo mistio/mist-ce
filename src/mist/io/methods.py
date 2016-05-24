001f3f03f3574dbf780b4828ea4d5bc56da21122
johnnyWalnut <johnnyWalnut@users.noreply.github.com>
2016-05-24 17:21:29 +0300

    Fix keys ids again, Keys - use real id instead of name (#763)
    * Rename key_id to key_name in function add_key()
    
    * Rename key_id tto key_name, keypair to key in function add_key
    
    * Edit docstring and pep8
    
    * In function delete_key(), rename keypair to key, change the query with id
    
    * In func delete_key() change the query with id, and add docstrings
    
    * In function delete_keys rename keypair to key
    
    * In function edit_key, rename old_id to keey_id, new_id to new_name
    
    * In function set_default_key rename keypair to key
    
    * In function set_default_key rename keypair to key, change the query with id
    
    * In function get_private_key, rename keypair to key, query by id
    
    * In function get_public_key, rename keypair to key, query by id
    
    * In funct generate_keypair rename keypair to key
    
    * In function assocciate_key(views and methods) rename keypair to key
    
    * In Shell.autoconfigure() make the key query by id
    
    * In func disassociate_key rename keypair to key
    
    * In func disassociate_key, make the key query by id
    
    * keypair to key
    
    * In func list_keys delete real_id and use the correct id and name
    
    * In func transform_key_machine_associations rename keypair to key
    
    * change id to name in keys list / add key form / single key page
    
    * Rename keypair to key in counterpart exceptions
    
    * Rename keypair's exceptions to key exceptions
    
    * Rename keypair exceptions to key
    
    * fix rename key / set default key / delte key
    
    * Fix edit key function, change query by id
    
    * Rename KeyPairNotFoundError to KeyNotFoundError
    
    * fix rename / delete key in single key view
    
    * fix unique key name validation for rename key func
    
    * fix keys selection and show name instead of id in add cloud / create machine / associate keys
    
    * refactor code that remove duplicate keys
    do keypair query by id
    change wrong string deleted to unauthorized
    
    * change id to name
    rename functions
    
    * change id to name, to be consistent with key's views functions
    
    * refactor code that removes duplicate keys
    change ddeleted to unauthorized
    do keypair query by id
    
    * fix filtering / sorting by using name instead of id
    
    * initialized tests submodule
    
    * fixed create_machine to use kei id instead of name
    
    * fixed methods.create machine to use key id
    
    * fix key data
    
    * send only the key id while creating cloud
    
    * fixed key query in bare metal and coreos and add cloud methods
    
    * small fix when key is not selected on add cloud
    
    * changed get_keypair_tags to get_key_tags in get public key method
    
    * fixed remaining get_keypair_tags calls

--- src/mist/io/methods.py
+++ src/mist/io/methods.py
@@ -92,7 +92,7 @@ def add_cloud(user, title, provider, apikey, apisecret, apiurl, tenant_name,
         if remove_on_error:
             if not machine_key:
                 raise RequiredParameterMissingError('machine_key')
-            Keypair.objects.get(owner=user, name=machine_key)
+            Keypair.objects.get(owner=user, id=machine_key)
             if not machine_user:
                 machine_user = 'root'
 
@@ -348,7 +348,7 @@ def _add_cloud_bare_metal(user, title, provider, params):
 
     use_ssh = remove_on_error and os_type == 'unix' and machine_key
     if use_ssh:
-        key = Keypair.objects.get(owner=user, name=machine_key)
+        key = Keypair.objects.get(owner=user, id=machine_key)
         if not machine_hostname:
             raise BadRequestError("You have specified an SSH key but machine "
                                   "hostname is empty.")
@@ -427,7 +427,7 @@ def _add_cloud_coreos(user, title, provider, params):
 
     use_ssh = remove_on_error and machine_key
     if use_ssh:
-        key = Keypair.objects.get(owner=user, name=machine_key)
+        key = Keypair.objects.get(owner=user, id=machine_key)
         if not machine_user:
             machine_user = 'root'
 
@@ -1037,49 +1037,48 @@ def delete_cloud(owner, cloud_id):
     trigger_session_update(owner, ['clouds'])
 
 
-def add_key(user, key_id, private_key):
-    """Adds a new keypair and returns the new key_id."""
+def add_key(user, key_name, private_key):
+    """Adds a new key by name and returns the new key_name."""
 
-    log.info("Adding key with id '%s'.", key_id)
-    if not key_id:
-        raise KeypairParameterMissingError(key_id)
+    log.info("Adding key with name '%s'.", key_name)
+
+    if not key_name:
+        raise KeyParameterMissingError(key_name)
     if not private_key:
         raise RequiredParameterMissingError("Private key is not provided")
-    key = Keypair.objects(owner=user, name=key_id)
+    key = Keypair.objects(owner=user, name=key_name)
     if key:
-        raise KeypairExistsError(key_id)
+        raise KeyExistsError(key_name)
 
-    keypair = Keypair()
-    keypair.private = private_key
-    keypair.name = key_id
-    keypair.construct_public_from_private()
+    key = Keypair()
+    key.private = private_key
+    key.name = key_name
+    key.construct_public_from_private()
     if not Keypair.objects(owner=user, default=True):
-        keypair.default = True
+        key.default = True
 
-    if not keypair.isvalid():
+    if not key.isvalid():
         raise KeyValidationError()
-    keypair.owner = user
-    keypair.save()
+    key.owner = user
+    key.save()
 
-    log.info("Added key with id '%s'", key_id)
+    log.info("Added key with name '%s'", key_name)
     trigger_session_update(user, ['keys'])
-    return key_id
+    return key_name
 
 
 def delete_key(user, key_id):
-    """Deletes given keypair.
+    """Deletes given key.
+    If key was default, then it checks if there are still keys left
+    and assigns another one as default.
 
-    If key was default, then it checks
-    if there are still keys left and assignes another one as default.
-
-    @param user: The User
-    @param key_id: The key_id to be deleted
-    @return: Returns nothing
-
+    :param user:
+    :param key_id:
+    :return:
     """
 
     log.info("Deleting key with id '%s'.", key_id)
-    key = Keypair.objects.get(owner=user, name=key_id)
+    key = Keypair.objects.get(owner=user, id=key_id)
     default_key = key.default
     # if key.default:
     #     default_key = key.default
@@ -1094,11 +1093,9 @@ def delete_key(user, key_id):
 
 def set_default_key(user, key_id):
     """Sets a new default key
-
-    @param user: The user
-    @param key_id: The id of the key we want to set as default
-    @return: Nothing. Raises only exceptions if needed.
-
+    :param user:
+    :param key_id:
+    :return:
     """
 
     log.info("Setting key with id '%s' as default.", key_id)
@@ -1108,46 +1105,54 @@ def set_default_key(user, key_id):
         default_key.default = False
         default_key.save()
 
-    key = Keypair.objects.get(owner=user, name=key_id)
+    key = Keypair.objects.get(owner=user, id=key_id)
     key.default = True
     key.save()
 
-    log.info("Succesfully set key with id '%s' as default.", key_id)
+    log.info("Successfully set key with id '%s' as default.", key_id)
     trigger_session_update(user, ['keys'])
 
 
-def edit_key(user, new_key, old_key):
+def edit_key(user, new_name, key_id):
+    """Edit name of an existing key.
+    Means rename key.
+    :param user:
+    :param new_name: the new key's name
+    :param key_id: the key's id
+    :return:
     """
-    Edits a given key's name from old_key ---> new_key
-    @param user: The User
-    @param new_key: The new Key name (id)
-    @param old_key: The old key name (id)
-    @return: Nothing, only raises exceptions if needed
+    if not new_name:
+        raise KeyParameterMissingError("new name")
 
-    """
+    key = Keypair.objects.get(owner=user, id=key_id)
 
-    log.info("Renaming key '%s' to '%s'.", old_key, new_key)
-    if not new_key:
-        raise KeypairParameterMissingError("new name")
+    log.info("Renaming key '%s' to '%s'.", key.name, new_name)
 
-    if old_key == new_key:
-        log.warning("Same name provided, will not edit key. No reason")
+    if key.name == new_name:
+        log.warning("Same name provided. No reason to edit this key")
         return
 
-    key = Keypair.objects.get(owner=user, name=old_key)
-    key.name = new_key
+    key.name = new_name
     key.save()
-    log.info("Renamed key '%s' to '%s'.", old_key, new_key)
+    log.info("Renamed key '%s' to '%s'.", key.name, new_name)
     trigger_session_update(user, ['keys'])
 
 
-def associate_key(user, key_id, cloud_id, machine_id, host='', username=None, port=22):
+def associate_key(user, key_id, cloud_id, machine_id,
+                  host='', username=None, port=22):
     """Associates a key with a machine.
 
     If host is set it will also attempt to actually deploy it to the
-    machine. To do that it requires another keypair (existing_key) that can
+    machine. To do that it requires another key (existing_key) that can
     connect to the machine.
-
+    :param user:
+    :param key_id:
+    :param cloud_id:
+    :param machine_id:
+    :param host:
+    :param username:
+    :param port:
+    :return:
     """
 
     log.info("Associating key %s to host %s", key_id, host)
@@ -1155,12 +1160,12 @@ def associate_key(user, key_id, cloud_id, machine_id, host='', username=None, po
         log.info("Host not given so will only create association without "
                  "actually deploying the key to the server.")
 
-    key = Keypair.objects.get(owner=user, name=key_id)
+    key = Keypair.objects.get(owner=user, id=key_id)
     cloud = Cloud.objects.get(owner=user, id=cloud_id)
     associated = False
     if Machine.objects(cloud=cloud, key_associations__keypair__exact=key,
                        machine_id=machine_id):
-        log.warning("Keypair '%s' already associated with machine '%s' "
+        log.warning("Key '%s' already associated with machine '%s' "
                     "in cloud '%s'", key_id, cloud_id, machine_id)
         associated = True
 
@@ -1168,7 +1173,7 @@ def associate_key(user, key_id, cloud_id, machine_id, host='', username=None, po
     # if not already associated, create the association
     # this is only needed if association doesn't exist and host is not provided
     # associations will otherwise be created by shell.autoconfigure upon
-    # succesful connection
+    # successful connection
     if not host:
         if not associated:
             try:
@@ -1199,46 +1204,54 @@ def associate_key(user, key_id, cloud_id, machine_id, host='', username=None, po
 
     try:
         # deploy key
-        ssh_command(user, cloud_id, machine_id, host, command, username=username, port=port)
+        ssh_command(user, cloud_id, machine_id, host,
+                    command, username=username, port=port)
     except MachineUnauthorizedError:
         # couldn't deploy key
         try:
             # maybe key was already deployed?
-            ssh_command(user, cloud_id, machine_id, host, 'uptime', key_id=key_id, username=username, port=port)
+            ssh_command(user, cloud_id, machine_id, host, 'uptime',
+                        key_id=key_id, username=username, port=port)
             log.info("Key was already deployed, local association created.")
         except MachineUnauthorizedError:
             # oh screw this
             raise MachineUnauthorizedError(
-                "Couldn't connect to deploy new SSH keypair."
+                "Couldn't connect to deploy new SSH key."
             )
     else:
         # deployment probably succeeded
-        # attemp to connect with new key
+        # attempt to connect with new key
         # if it fails to connect it'll raise exception
         # there is no need to manually set the association in keypair.machines
         # that is automatically handled by Shell, if it is configured by
         # shell.autoconfigure (which ssh_command does)
-        ssh_command(user, cloud_id, machine_id, host, 'uptime', key_id=key_id, username=username, port=port)
-        log.info("Key associated and deployed succesfully.")
+        ssh_command(user, cloud_id, machine_id, host, 'uptime',
+                    key_id=key_id, username=username, port=port)
+        log.info("Key associated and deployed successfully.")
 
 
 def disassociate_key(user, key_id, cloud_id, machine_id, host=None):
     """Disassociates a key from a machine.
-
     If host is set it will also attempt to actually remove it from
     the machine.
 
+    :param user:
+    :param key_id:
+    :param cloud_id:
+    :param machine_id:
+    :param host:
+    :return:
     """
 
     log.info("Disassociating key, undeploy = %s" % host)
-    key = Keypair.objects.get(owner=user, name=key_id)
+    key = Keypair.objects.get(owner=user, id=key_id)
     cloud = Cloud.objects.get(owner=user, id=cloud_id)
     machine = Machine.objects.get(cloud=cloud,
                                   key_associations__keypair__exact=key,
                                   machine_id=machine_id)
     # key not associated
     if not machine:
-        raise BadRequestError("Keypair '%s' is not associated with "
+        raise BadRequestError("Key '%s' is not associated with "
                               "machine '%s'" % (key_id, machine_id))
 
     if host:
@@ -1654,7 +1667,7 @@ def create_machine(user, cloud_id, key_id, machine_name, location_id,
     machine_name = machine_name_validator(conn.type, machine_name)
     key = None
     if key_id:
-        key = Keypair.objects.get(owner=user, name=key_id)
+        key = Keypair.objects.get(owner=user, id=key_id)
 
     # if key_id not provided, search for default key
     if conn.type not in [Provider.LIBVIRT, Provider.DOCKER]:
@@ -2903,6 +2916,10 @@ def list_clouds(user):
 
 
 def list_keys(user):
+    """List user's keys
+    :param user:
+    :return:
+    """
     keys = Keypair.objects(owner=user).only("default", "name")
     clouds = Cloud.objects(owner=user)
     key_objects = []
@@ -2910,8 +2927,8 @@ def list_keys(user):
         key_object = {}
         machines = Machine.objects(cloud__in=clouds,
                                    key_associations__keypair__exact=key)
-        key_object["id"] = key.name # This is for backwards compatibility
-        key_object['_real_id'] = key.id  # Fuck backwards compatibility
+        key_object["id"] = key.id
+        key_object['name'] = key.name
         key_object["isDefault"] = key.default
         key_object["machines"] = transform_key_machine_associations(machines,
                                                                     key)
