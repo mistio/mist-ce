b0498424c3a9a1d8aa63546857aacd186a1f0f30
ptzianos <pavlos.tzianos@gmail.com>
2016-07-22 19:07:29 +0300

    fixed setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ import os
 
 from setuptools import setup, find_packages
 
-here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.txt')).read()
-CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(BASEDIR, 'README.txt')).read()
+CHANGES = open(os.path.join(BASEDIR, 'CHANGES.txt')).read()
 
-requires = []
+with open(os.path.join(BASEDIR, 'requirements.txt')) as fobj:
+    REQUIRES = [line.strip() for line in fobj.readlines()
+                if not line.startswith('#')]
 
 setup(name='mist.io',
       version='0.9.9',
@@ -29,8 +31,8 @@ setup(name='mist.io',
       namespace_packages=['mist'],
       include_package_data=True,
       zip_safe=False,
-      install_requires=requires,
-      tests_require=requires,
+      install_requires=REQUIRES,
+      tests_require=REQUIRES,
       test_suite="mist.io",
       entry_points="""\
       [paste.app_factory]
