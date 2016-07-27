a88ff00d4596ea70589b14ea38d65c895c8e8255
Dimitris Rozakis <dr@mist.io>
2016-07-27 16:40:18 +0300

    Make decorator for cloud unit tests
    And print tested cloud's class name when running `py.test -v`.

--- unit-tests/test_clouds.py
+++ unit-tests/test_clouds.py
@@ -18,21 +18,28 @@ ORG = Organization(name=uuid.uuid4().hex)
 CLOUDS = [cdict.pop('model')(owner=ORG, **cdict) for cdict in SETTINGS]
 
 
-@pytest.mark.parametrize('cloud', CLOUDS)
+def iter_clouds(test_func):
+    """Iterate test function over CLOUDS"""
+    return pytest.mark.parametrize(
+        'cloud', CLOUDS, ids=lambda cloud: cloud.__class__.__name__
+    )(test_func)
+
+
+@iter_clouds
 def test_list_machines(cloud):
     print len(cloud.ctl.list_machines())
 
 
-@pytest.mark.parametrize('cloud', CLOUDS)
+@iter_clouds
 def test_list_locations(cloud):
     print len(cloud.ctl.list_locations())
 
 
-@pytest.mark.parametrize('cloud', CLOUDS)
+@iter_clouds
 def test_list_images(cloud):
     print len(cloud.ctl.list_images())
 
 
-@pytest.mark.parametrize('cloud', CLOUDS)
+@iter_clouds
 def test_list_sizes(cloud):
     print len(cloud.ctl.list_sizes())
