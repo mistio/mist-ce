16a4531a15df755edf7a7438c576052aaf53746d
Christos <pollalis.ch@gmail.com>
2016-10-26 23:25:22 +0300

    Update unittests with the new subcontroller

--- unit-tests/test_clouds.py
+++ unit-tests/test_clouds.py
@@ -43,7 +43,7 @@ def compare_fields(res):
 
 def test_list_machines(cloud, load_staging_l_machines):
 
-    response = cloud.ctl.list_machines()
+    response = cloud.ctl.compute.list_machines()
     print len(response)
 
     if response:
@@ -69,7 +69,7 @@ def test_list_machines(cloud, load_staging_l_machines):
 
 def test_list_locations(cloud, load_staging_l_locations):
 
-    response = cloud.ctl.list_locations()
+    response = cloud.ctl.compute.list_locations()
     print len(response)
 
     if response:
@@ -80,7 +80,7 @@ def test_list_locations(cloud, load_staging_l_locations):
 
 def test_list_images(cloud, load_staging_l_images):
 
-    response = cloud.ctl.list_images()
+    response = cloud.ctl.compute.list_images()
     print len(response)
 
     if response:
@@ -91,7 +91,7 @@ def test_list_images(cloud, load_staging_l_images):
 
 def test_list_sizes(cloud, load_staging_l_sizes):
 
-    response = cloud.ctl.list_sizes()
+    response = cloud.ctl.compute.list_sizes()
     print len(response)
 
     if response:
