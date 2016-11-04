210e95a282e63ecf795624211dce7fd6dcd1c5fa
johnnyWalnut <ikarydis0@gmail.com>
2016-11-04 18:01:28 +0200

    delete unused imports move def  trigger session update in views pep8

--- src/mist/io/schedules/controllers.py
+++ src/mist/io/schedules/controllers.py
@@ -5,16 +5,11 @@ import mongoengine as me
 from mist.core.cloud.models import Machine
 from mist.core.script.models import Script
 import mist.io.schedules.models as schedules
-from mist.io.helpers import trigger_session_update
-
 from mist.io.exceptions import NotFoundError
-from mist.core.exceptions import InvalidSchedule
 from mist.core.exceptions import BadRequestError
 from mist.core.exceptions import ScriptNotFoundError
-from mist.core.exceptions import PeriodicTaskNotFound
 from mist.core.exceptions import ScheduleOperationError
 from mist.core.exceptions import ScheduleNameExistsError
-from mist.core.exceptions import RequiredParameterMissingError
 
 log = logging.getLogger(__name__)
 
@@ -29,10 +24,9 @@ class ScheduleController(object):
           schedule = mist.io.schedules.models.Schedule.objects.get(id=s_id)
           schedule.ctl.add_entry
         """
-
         self.schedule = schedule
 
-    def add(self, auth_context, **params):
+    def add(self, auth_context, **kwargs):
         """Add an entry to the database
 
         This is only to be called by `Schedule.add` classmethod to create
@@ -41,39 +35,40 @@ class ScheduleController(object):
 
         """
 
-        # check params
-        if not (params.get('script_id', '') or params.get('action', '')):
-            raise BadRequestError(
-                "You must provide script_id or machine's action")
+        # check if one of these pairs exist
+        # script_id/action and machines_uuids/ machines_tags
+        if not (kwargs.get('script_id', '') or kwargs.get('action', '')):
+            raise BadRequestError("You must provide script_id "
+                                  "or machine's action")
 
-        if not (params.get('machines_uuids') or params.get('machines_tags')):
+        if not (kwargs.get('machines_uuids') or kwargs.get('machines_tags')):
             raise BadRequestError(
                 "You must provide a list of machine ids or tags")
 
         try:
-            self.update(auth_context, **params)
-        except me.ValidationError as exc:
+            self.update(auth_context, **kwargs)
+        except me.ValidationError:
             # Propagate original error.
             raise
         return self.schedule.id
 
-    def update(self, auth_context, **params):
+    def update(self, auth_context, **kwargs):
 
         """Edit an existing Schedule
         """
         owner = auth_context.owner
 
-        script_id = params.get('script_id', '')
-        action = params.get('action', '')
+        script_id = kwargs.get('script_id', '')
+        action = kwargs.get('action', '')
 
         if action not in ['', 'reboot', 'destroy', 'start', 'stop']:
             raise BadRequestError("Action is not correct")
 
         if (isinstance(self.schedule.task_type, schedules.ScriptTask)
-            and action):
+                and action):
             raise BadRequestError("You cannot change from script to action")
         if (isinstance(self.schedule.task_type, schedules.ActionTask)
-            and script_id):
+                and script_id):
             raise BadRequestError("You cannot change from action to script")
 
         if script_id:
@@ -85,8 +80,8 @@ class ScheduleController(object):
             # SEC require permission RUN on script
             auth_context.check_perm('script', 'run', script_id)
 
-        machines_uuids = params.get('machines_uuids', '')
-        machines_tags = params.get('machines_tags', '')
+        machines_uuids = kwargs.get('machines_uuids', '')
+        machines_tags = kwargs.get('machines_tags', '')
 
         # convert machines' uuids to machine objects
         # and check permissions
@@ -123,7 +118,7 @@ class ScheduleController(object):
                 auth_context.check_perm("machine", "run_script", None)
 
         # set schedule attributes
-        for key, value in params.iteritems():
+        for key, value in kwargs.iteritems():
             if key in self.schedule._fields.keys():
                 setattr(self.schedule, key, value)
 
@@ -140,7 +135,7 @@ class ScheduleController(object):
         elif script_id:
             self.schedule.task_type = schedules.ScriptTask(script_id=script_id)
 
-        schedule_type = params.get('schedule_type')
+        schedule_type = kwargs.get('schedule_type')
         if schedule_type not in ['crontab', 'interval', 'one_off', None]:
             raise BadRequestError('schedule type must be one of these '
                                   '(crontab, interval, one_off)]')
@@ -148,16 +143,16 @@ class ScheduleController(object):
         future_date = None
         if (schedule_type == 'crontab' or
                 isinstance(self.schedule.schedule_type, schedules.Crontab)):
-            future_date = params.get('expires',
+            future_date = kwargs.get('expires',
                                      str(self.schedule.expires)) or ''
 
         elif (schedule_type == 'interval' or
                 isinstance(self.schedule.schedule_type, schedules.Interval)):
-            future_date = params.get('expires',
+            future_date = kwargs.get('expires',
                                      str(self.schedule.expires)) or ''
 
         elif schedule_type == 'one_off':
-            future_date = params.get('schedule_entry', '')
+            future_date = kwargs.get('schedule_entry', '')
             if not future_date:
                 raise BadRequestError('one_off schedule requires date '
                                       'given in schedule_entry')
@@ -174,7 +169,7 @@ class ScheduleController(object):
                                       'Please contact Marty McFly')
 
         if schedule_type == 'crontab':
-            schedule_entry = json.loads(params.get('schedule_entry', '{}'))
+            schedule_entry = json.loads(kwargs.get('schedule_entry', '{}'))
             for k in schedule_entry.keys():
                 if k not in ['minute', 'hour', 'day_of_week', 'day_of_month',
                              'month_of_year']:
@@ -182,7 +177,7 @@ class ScheduleController(object):
             self.schedule.schedule_type = schedules.Crontab(**schedule_entry)
 
         elif schedule_type == 'interval':
-            schedule_entry = json.loads(params.get('schedule_entry', '{}'))
+            schedule_entry = json.loads(kwargs.get('schedule_entry', '{}'))
             for k in schedule_entry.keys():
                 if k not in ['period', 'every']:
                     raise BadRequestError("Invalid key given: %s" % k)
@@ -200,11 +195,12 @@ class ScheduleController(object):
         try:
             self.schedule.save()
         except me.ValidationError as e:
+            log.error("Error updating %s: %s", self.schedule.name,
+                      e.ti_dict())
             raise BadRequestError({"msg": e.message, "errors": e.to_dict()})
         except me.NotUniqueError:
             raise ScheduleNameExistsError()
         except me.OperationError:
             raise ScheduleOperationError()
 
-        trigger_session_update(owner, ['schedules'])
         return self.schedule.id
