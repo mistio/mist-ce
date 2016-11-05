4c95dca75f3d76559556e4d5cdf3f10d83f7e21f
Jason Kolios <jason.kolios@gmail.com>
2016-11-05 15:46:19 +0200

    WIP: DB object creation and sync process

--- src/mist/io/clouds/network/base.py
+++ src/mist/io/clouds/network/base.py
@@ -15,13 +15,26 @@ class NetworkController(object):
         """Create a new network"""
         network_info = self._create_network(network, subnet, router)
 
-        # Persist a new Network object to the DB
-        network_entry = Network(title=network_info['network']['name'],
-                                libcloud_id=network_info['network']['id'],
-                                cloud=self.ctl.cloud)
-        network_entry.save()
+        network_object = self._create_network_db_object(network_info, do_save=False)
 
+        for subnet in network_info['subnets']:
+            subnet_object = self._create_subnet_db_object(subnet, do_save=False)
+            subnet_object.base_network = network_object
+            network_object.subnets.append(subnet_object)
+            subnet_object.save()
+
+        network_object.save()
         return network_info
+
+    def _create_network_db_object(self, network_info, do_save=True):
+        """ Persist a new Network object to the DB"""
+        network_object = Network(title=network_info['network']['name'],
+                                 libcloud_id=network_info['network']['id'],
+                                 cloud=self.ctl.cloud)
+        if do_save:
+            network_object.save()
+
+        return network_object
 
     def _create_network(self, network, subnet, router):
         """Handles cloud-specific network creation calls.
@@ -32,19 +45,9 @@ class NetworkController(object):
     def create_subnet(self, subnet, parent_network_id):
         """Create a new subnet"""
 
-        parent_network_entry = Network.objects.get(id=parent_network_id)
-
         subnet_info = self._create_subnet(subnet, parent_network_id)
+        self._create_subnet_db_object(subnet_info)
 
-        # Persist a new Subnet object to the DB
-        subnet_entry = Subnet(title=subnet_info['name'],
-                              libcloud_id=subnet_info['id'],
-                              cloud=self.ctl.cloud,
-                              base_network=parent_network_entry)
-        subnet_entry.save()
-
-        parent_network_entry.subnets.append(subnet_entry)
-        parent_network_entry.save()
         return subnet_info
 
     def _create_subnet(self, subnet, parent_network):
@@ -53,11 +56,26 @@ class NetworkController(object):
 
         raise NotImplementedError()
 
+    def _create_subnet_db_object(self, subnet_info, do_save=True):
+        """ Persist a new Subnet object to the DB """
+        subnet_object = Subnet(title=subnet_info['name'],
+                               libcloud_id=subnet_info['id'],
+                               cloud=self.ctl.cloud)
+        if do_save:
+            subnet_object.save()
+
+        return subnet_object
+
     def list_networks(self, return_format):
         """List Networks"""
 
         libcloud_networks = self.ctl.connection.ex_list_networks()
         network_info = self._parse_network_listing(libcloud_networks, return_format)
+
+
+
+        # TODO: Fetch all existing Network and subnet objects for this cloud, compare and sync
+
         return network_info
 
     def _parse_network_listing(self, network_listing, return_format):
