c9c75c3c24afd3b8bb9a0783d7c5861ac367d655
Konstantinos Tsaprailis <kostis.tsaprailis@gmail.com>
2016-11-05 21:11:10 +0200

    This is a temp commit while pulling the subcontrollers.dr branch

--- src/mist/io/clouds/main/base.py
+++ src/mist/io/clouds/main/base.py
@@ -1,18 +1,22 @@
-"""Definition of the base class for Clouds
+"""Definition of base main controllers for clouds
 
-The `BaseController` contains the basic functionality for a given cloud,
-such as adding, deleting, or editing. Cloud-specific controllers are in
-`mist.io.clouds.main.controllers`.
+This currently contains only BaseMainController. It includes basic
+functionality common to all clouds, such as add, update, rename, disable etc.
+
+The main controller also acts as a gateway to specific controllers. For
+example, one may do
+
+    cloud.ctl.enable()
+    cloud.ctl.compute.list_machines()
+
+Cloud specific main controllers are in
+`mist.io.clouds.controllers.main.controllers`.
 
 """
 
-
-import ssl
 import logging
 
 import mongoengine as me
-
-from libcloud.common.types import InvalidCredsError
 
 from mist.io.exceptions import MistError
 from mist.io.exceptions import BadRequestError
@@ -21,57 +25,35 @@ from mist.io.exceptions import InternalServerError
 from mist.io.exceptions import CloudUnavailableError
 from mist.io.exceptions import CloudUnauthorizedError
 
+from mist.io.clouds.controllers.compute.base import BaseComputeController
+from mist.io.clouds.utils import rename_kwargs
+
 # from mist.core.cloud.models import Machine
 
 
 log = logging.getLogger(__name__)
 
 
-# TODO: This should be moved to the tags directory, once it's migrated.
-def tags_to_dict(tags):
-    """Return a dict with each key/value tag being a dict item
+class BaseMainController(object):
+    """Base main controller class for all cloud types
 
-    This will handle:
-    - dict {key1: value1, key2: value2, ...}
-    - lists of {key: value} pairs
-    - lists of {"key": key, "value": value} pairs, value field is optional
+    BaseMainController defines common cloud operations, such as add, update,
+    disable, that mainly affect mist, instead of interacting with the remote
+    cloud itself. These operations are mostly the same for all different
+    clouds.
 
-    It will return:
-    dict {key1: value1, key2: value2, ...}
+    Main controllers act as a gateway to specific controllers. For example, one
+    may do
 
-    """
+        cloud.ctl.enable()
+        cloud.ctl.compute.list_machines()
 
-    if isinstance(tags, dict):
-        return tags
-    tdict = {}
-    for tag in tags:
-        if isinstance(tag, dict):
-            if len(tag) == 1:
-                key = tag.keys()[0]
-                tdict[tag] = tag[key]
-            elif 'key' in tag:
-                tdict[tag['key']] = tag.get('value')
-    return tdict
+    For this to work, subclasses must define the appropriate subcontroller
+    class, by defining for example a `ComputeController` attribute with a
+    subclass of mist.io.clouds.controllers.compute.base.BaseComputeController.
 
-
-def rename_kwargs(kwargs, old_key, new_key):
-    """Given a `kwargs` dict rename `old_key` to `new_key`"""
-    if old_key in kwargs:
-        if new_key not in kwargs:
-            log.warning("Got param '%s' when expecting '%s', trasforming.",
-                        old_key, new_key)
-            kwargs[new_key] = kwargs.pop(old_key)
-        else:
-            log.warning("Got both param '%s' and '%s', will not tranform.",
-                        old_key, new_key)
-
-
-class BaseController(object):
-    """Abstract base class for every cloud/provider controller
-
-    This base controller factors out all the basic steps common to all or
-    most clouds into a base class, and defines an interface for provider or
-    technology specific cloud controllers.
+    For specific clouds, main controllers are defined in
+    `mist.io.clouds.controllers.main.controllers`.
 
     Subclasses are meant to extend or override methods of this base class to
     account for differencies between different cloud types.
@@ -81,40 +63,24 @@ class BaseController(object):
     feasible. That is to say, don't add a new method to a subclass unless
     there is a very good reason to do so.
 
-    The following convention is followed:
-
     Any methods and attributes that don't start with an underscore are the
     controller's public API.
 
-    In the `BaseController`, these public methods will in most cases contain
-    a basic implementation that works for most clouds, along with the proper
-    logging and error handling. In almost all cases, subclasses SHOULD NOT
-    override or extend the public methods of `BaseController`. To account for
-    cloud/subclass specific behaviour, one is expected to override the
-    internal/private methods of `BaseController`.
-
-    Any methods and attributes that start with an underscore are the
-    controller's internal/private API.
-
-    To account for cloud/subclass specific behaviour, the public methods of
-    `BaseController` call a number of private methods. These methods will
-    always start with an underscore, such as `self._connect`. When an internal
-    method is only ever used in the process of one public method, it is
-    prefixed as such to make identification and purpose more obvious. For
-    example, method `self._add__prepare_kwargs` is called in order to apply
-    any special preprocessing of the params passed to `self.add`.
-
-    This `BaseController` defines a strict interface to controlling clouds.
-    For each different cloud type, a subclass needs to be defined. The subclass
-    must at least define a proper `self._connect` method. For simple clouds,
-    this may be enough. To provide cloud specific processing, hook the code on
-    the appropriate private method. Each method defined here documents its
-    intended purpose and use.
+    In the `BaseMainController`, these public methods will in most cases
+    contain a basic implementation that works for most clouds, along with the
+    proper logging and error handling. In almost all cases, subclasses SHOULD
+    NOT override or extend the public methods of `BaseMainController`. To
+    account for cloud/subclass specific behaviour, one is expected to override
+    the internal/private methods of `BaseMainController`.
 
     """
 
+    ComputeController = None
+    # DnsController = None
+    # NetworkController = None
+
     def __init__(self, cloud):
-        """Initialize cloud controller given a cloud
+        """Initialize main cloud controller given a cloud
 
         Most times one is expected to access a controller from inside the
         cloud, like this:
@@ -124,213 +90,27 @@ class BaseController(object):
 
         Subclasses SHOULD NOT override this method.
 
-        If a subclass has to initialize a certain instance attribute, it SHOULD
+        If a subclass has to initialize a certain instance attribute, it MAY
         extend this method instead.
 
         """
+
         self.cloud = cloud
         self._conn = None
-        self._dns_conn = None
 
-    @property
-    def connection(self):
-        """Cached libcloud connection, accessible as attribute
+        # Initialize compute controller.
+        assert issubclass(self.ComputeController, BaseComputeController)
+        self.compute = self.ComputeController(self)
 
-        Subclasses SHOULD NOT have to override or extend this method.
+        # TODO: Initialize dns controller.
+        # if self.DnsController is not None:
+        #     assert issubclass(self.DnsController, DnsController)
+        #     self.dns = self.DnsController(self)
 
-        """
-        if self._conn is None:
-            self._conn = self.connect()
-        return self._conn
-
-    def connect(self):
-        """Return libcloud-like connection to cloud
-
-        This is a wrapper, an error handler, around cloud specific `_connect`
-        methods.
-
-        Subclasses SHOULD NOT override or extend this method.
-
-        Instead, subclasses MUST override `_connect` method.
-
-        """
-        try:
-            return self._connect()
-        except (CloudUnavailableError, CloudUnauthorizedError) as exc:
-            log.error("Error adding cloud %s: %r", self.cloud, exc)
-            raise
-        except InvalidCredsError as exc:
-            log.warning("Invalid creds while connecting to %s: %s",
-                        self.cloud, exc)
-            raise CloudUnauthorizedError("Invalid creds.")
-        except ssl.SSLError as exc:
-            log.error("SSLError on connecting to %s: %s", self.cloud, exc)
-            raise CloudUnavailableError(exc=exc)
-        except Exception as exc:
-            log.exception("Error while connecting to %s", self.cloud)
-            raise CloudUnavailableError(exc=exc)
-
-    def _connect(self):
-        """Return libcloud-like connection to cloud
-
-        This is called solely by `connect` which adds error handling.
-
-        All subclasses MUST implement this method.
-
-        """
-        raise NotImplementedError()
-
-    def check_connection(self):
-        """Raise exception if we can't connect to cloud provider
-
-        In case of error, an instance of `CloudUnavailableError` or
-        `CloudUnauthorizedError` should be raised.
-
-        For most cloud providers, who use an HTTP API, calling `connect`
-        doesn't really establish a connection, so we also have to attempt to
-        make an actual call such as `list_machines` to verify that the
-        connection actually works.
-
-        If a subclass's `connect` not raising errors is enough to make sure
-        that establishing a connection works, then these subclasses should
-        override this method and only call `connect`.
-
-        In most cases, subclasses SHOULD NOT override or extend this method.
-
-        """
-        self.connect()
-        self.compute.list_machines()
-
-    def disconnect(self):
-        """Close libcloud-like connection to cloud
-
-        If a connection object has been initialized, this method will attempt
-        to call its disconnect method.
-
-        This method is automatically called by the class's destructor.
-        This may however be unreliable, so users should call `disconnect`
-        manually to be on the safe side.
-
-        For cloud providers whose connection object is dummy in the sense that
-        it doesn't represent an actual underlying connection, this method
-        doesn't really do anything.
-
-        Subclasses SHOULD NOT override this method.
-
-        If a subclass has to perform some special clean up, like deleting
-        temporary files, it SHOULD *extend* this method instead.
-
-        """
-        if self._conn is not None:
-            log.debug("Closing libcloud-like connection for %s.", self.cloud)
-            try:
-                self._conn.disconnect()
-            except AttributeError:
-                pass
-            except Exception as exc:
-                log.error("Error disconnecting cloud '%s': %r", self, exc)
-            self._conn = None
-
-    @property
-    def dns_connection(self):
-        """Cached libcloud connection, accessible as attribute
-
-        Subclasses SHOULD NOT have to override or extend this method.
-
-        """
-        if self._dns_conn is None:
-            self._dns_conn = self.dns_connect()
-        return self._dns_conn
-
-    def dns_connect(self):
-        """Return libcloud-like connection to cloud DNS provider
-
-        This is a wrapper, an error handler, around cloud specific `_dns_connect`
-        methods.
-
-        Subclasses SHOULD NOT override or extend this method.
-
-        Instead, subclasses MUST override `_dns_connect` method.
-
-        """
-        try:
-            return self._dns_connect()
-        except (CloudUnavailableError, CloudUnauthorizedError) as exc:
-            log.error("Error adding DNS cloud %s: %r", self.cloud, exc)
-            raise
-        except InvalidCredsError as exc:
-            log.warning("Invalid creds while connecting to DNS %s: %s",
-                        self.cloud, exc)
-            raise CloudUnauthorizedError("Invalid creds.")
-        except ssl.SSLError as exc:
-            log.error("SSLError on connecting to DNS %s: %s", self.cloud, exc)
-            raise CloudUnavailableError(exc=exc)
-        except Exception as exc:
-            log.exception("Error while connecting to DNS %s", self.cloud)
-            raise CloudUnavailableError(exc=exc)
-
-    def _dns_connect(self):
-        """Return libcloud-like connection to cloud DNS provider
-
-        This is called solely by `dns_connect` which adds error handling.
-
-        All subclasses MUST implement this method.
-
-        """
-        raise NotImplementedError()
-
-    def check_dns_connection(self):
-        """Raise exception if we can't connect to cloud provider
-
-        In case of error, an instance of `CloudUnavailableError` or
-        `CloudUnauthorizedError` should be raised.
-
-        For most cloud providers, who use an HTTP API, calling `connect`
-        doesn't really establish a connection, so we also have to attempt to
-        make an actual call such as `list_machines` to verify that the
-        connection actually works.
-
-        If a subclass's `connect` not raising errors is enough to make sure
-        that establishing a connection works, then these subclasses should
-        override this method and only call `connect`.
-
-        In most cases, subclasses SHOULD NOT override or extend this method.
-
-        """
-        self.dns_connect()
-        # self.dns.list_zones()
-
-    def dns_disconnect(self):
-        """Close libcloud-like connection to cloud
-
-        If a connection object has been initialized, this method will attempt
-        to call its disconnect method.
-
-        This method is automatically called by the class's destructor.
-        This may however be unreliable, so users should call `disconnect`
-        manually to be on the safe side.
-
-        For cloud providers whose connection object is dummy in the sense that
-        it doesn't represent an actual underlying connection, this method
-        doesn't really do anything.
-
-        Subclasses SHOULD NOT override this method.
-
-        If a subclass has to perform some special clean up, like deleting
-        temporary files, it SHOULD *extend* this method instead.
-
-        """
-        if self._dns_conn is not None:
-            log.debug("Closing libcloud-like connection for DNS %s.",
-                self.cloud)
-            try:
-                self._dns_conn.disconnect()
-            except AttributeError:
-                pass
-            except Exception as exc:
-                log.error("Error disconnecting DNS cloud '%s': %r", self, exc)
-            self._dns_conn = None
-
+        # TODO: Initialize network controller.
+        # if self.NetworkController is not None:
+        #     assert issubclass(self.NetworkController, NetworkController)
+        #     self.network = self.NetworkController(self)
 
     def add(self, fail_on_error=True, fail_on_invalid_params=True, **kwargs):
         """Add new Cloud to the database
@@ -468,7 +248,7 @@ class BaseController(object):
         # Try to connect to cloud.
         if fail_on_error:
             try:
-                self.check_connection()
+                self.compute.check_connection()
             except (CloudUnavailableError, CloudUnauthorizedError) as exc:
                 log.error("Will not update cloud %s because "
                           "we couldn't connect: %r", self.cloud, exc)
@@ -519,6 +299,5 @@ class BaseController(object):
         self.cloud.enabled = False
         self.cloud.save()
 
-    def __del__(self):
-        """Disconnect libcloud connection upon garbage collection"""
-        self.disconnect()
+    def disconnect(self):
+        self.compute.disconnect()
