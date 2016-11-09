9942b46774d2ccc8aa7e5aa17508c95f7bb0afb1
Jason Kolios <jason.kolios@gmail.com>
2016-11-09 15:10:02 +0200

    Refactored Network and Subnet creation to be part of the model classes.

--- src/mist/io/clouds/controllers/network/base.py
+++ src/mist/io/clouds/controllers/network/base.py
@@ -1,8 +1,5 @@
 import logging
-import copy
 import itertools
-
-import mongoengine as me
 
 from mist.io.clouds.controllers.base import BaseController
 from mist.io.clouds.controllers.network.models import Network, Subnet
@@ -26,46 +23,22 @@ class BaseNetworkController(BaseController):
         Should not be overridden or extended."""
         network_info, libcloud_network, libcloud_subnet = self._create_network(network, subnet, router)
 
-        network_object = self._create_network_db_object(libcloud_network=libcloud_network, do_save=False)
+        network_object = Network.create(self.cloud,
+                                        libcloud_network=libcloud_network,
+                                        do_save=False)
 
         if libcloud_subnet:
-            subnet_object = self._create_subnet_db_object(libcloud_subnet=libcloud_subnet,
-                                                          parent_network=network_object,
-                                                          do_save=False)
+            subnet_object = Subnet.create(
+                self.cloud,
+                libcloud_subnet=libcloud_subnet,
+                parent_network=network_object,
+                do_save=False)
             subnet_object.base_network = network_object
             network_object.subnets.append(subnet_object)
             subnet_object.save()
 
         network_object.save()
         return network_info
-
-    def _create_network_db_object(self, libcloud_network=None, network_info=None, do_save=True):
-        """ Persists a new Network object to the DB.
-        Should not be overridden or extended by subclasses"""
-
-        if not libcloud_network and not network_info:
-            raise RequiredParameterMissingError('libcloud_network ot network_info')
-
-        if libcloud_network:
-            network_object = Network(title=libcloud_network.name,
-                                     libcloud_id=libcloud_network.id,
-                                     cloud=self.ctl.cloud,
-                                     extra=copy.copy(libcloud_network.extra))
-        else:
-            network_object = Network(title=network_info['name'],
-                                     libcloud_id=network_info['id'],
-                                     cloud=self.ctl.cloud,
-                                     extra={key: value for key, value in network_info.items()
-                                            if key not in ['name', 'id']})
-        if do_save:
-            try:
-                network_object.save()
-            except me.ValidationError as exc:
-                log.error("Error adding network: %s: %s", libcloud_network.name, exc.to_dict())
-                raise BadRequestError({"msg": exc.message,
-                                       "errors": exc.to_dict()})
-
-        return network_object
 
     def _create_network(self, network, subnet, router):
         """Handles cloud-specific network creation calls.
@@ -83,47 +56,17 @@ class BaseNetworkController(BaseController):
 
         libcloud_subnet = self._create_subnet(subnet, parent_network_id)
         parent_network = Network.objects.get(id=parent_network_id)
-        self._create_subnet_db_object(libcloud_subnet=libcloud_subnet,
+        subnet_object = Subnet.create(self.cloud,
+                                      libcloud_subnet=libcloud_subnet,
                                       parent_network=parent_network)
 
-        return libcloud_subnet
+        return subnet_object, libcloud_subnet
 
     def _create_subnet(self, subnet, parent_network):
         """Handles cloud-specific subnet creation calls.
         Should be overridden by all subclasses that can support it"""
 
         raise NotImplementedError()
-
-    def _create_subnet_db_object(self, parent_network, libcloud_subnet=None, subnet_info=None, do_save=True):
-        """ Persist a new Subnet object to the DB
-            Should not be overridden or extended by subclasses"""
-
-        if not libcloud_subnet and not subnet_info:
-            raise RequiredParameterMissingError('libcloud_subnet ot subnet_info')
-
-        if libcloud_subnet:
-            subnet_object = Subnet(title=libcloud_subnet.name,
-                                   libcloud_id=libcloud_subnet.id,
-                                   cloud=self.ctl.cloud,
-                                   base_network=parent_network,
-                                   extra=copy.copy(libcloud_subnet.extra))
-        else:
-            subnet_object = Subnet(title=subnet_info['name'],
-                                   libcloud_id=subnet_info['id'],
-                                   cloud=self.ctl.cloud,
-                                   base_network=parent_network,
-                                   extra={key: value for key, value in subnet_info.items()
-                                          if key not in ['name', 'id']})
-
-        if do_save:
-            try:
-                subnet_object.save()
-            except me.ValidationError as exc:
-                log.error("Error adding subnet: %s: %s", libcloud_subnet.name, exc.to_dict())
-                raise BadRequestError({"msg": exc.message,
-                                       "errors": exc.to_dict()})
-
-        return subnet_object
 
     def list_networks(self, return_format):
         """List all Networks present on the cloud. Also syncs the state of the Network and Subnet documents on the DB
@@ -150,8 +93,10 @@ class BaseNetworkController(BaseController):
             try:
                 db_network = Network.objects.get(cloud=self.ctl.cloud, libcloud_id=network['id'])
             except Network.DoesNotExist:
-                db_network = self._create_network_db_object(libcloud_network=None, network_info=network,
-                                                            do_save=False)
+                db_network = Network.create(self.cloud,
+                                            libcloud_network=None,
+                                            network_info=network,
+                                            do_save=False)
 
             # Update the attributes on the DB network object
             db_network.name = network['name']
@@ -164,9 +109,11 @@ class BaseNetworkController(BaseController):
                 try:
                     db_subnet = Subnet.objects.get(cloud=self.ctl.cloud, libcloud_id=subnet['id'])
                 except Subnet.DoesNotExist:
-                    db_subnet = self._create_subnet_db_object(subnet_info=subnet,
-                                                              parent_network=db_network,
-                                                              do_save=False)
+                    db_subnet = Subnet.create(
+                        self.cloud,
+                        subnet_info=subnet,
+                        parent_network=db_network,
+                        do_save=False)
 
                 # Update the attributes on the DB network object
                 db_subnet.name = subnet['name']
--- src/mist/io/clouds/controllers/network/models.py
+++ src/mist/io/clouds/controllers/network/models.py
@@ -1,5 +1,12 @@
-import mongoengine as me
 import uuid
+import copy
+import logging
+
+import mongoengine as me
+
+from mist.io.exceptions import ConflictError, BadRequestError, RequiredParameterMissingError
+
+log = logging.getLogger(__name__)
 
 
 class Network(me.Document):
@@ -24,6 +31,35 @@ class Network(me.Document):
                                                  cloud=self.cloud,
                                                  cloud_id=self.libcloud_id)
 
+    @classmethod
+    def create(cls, cloud, libcloud_network=None, network_info=None, do_save=True):
+        """ Persists a new Network object to the DB.
+        Should not be overridden or extended by subclasses"""
+
+        if not libcloud_network and not network_info:
+            raise RequiredParameterMissingError('libcloud_network ot network_info')
+
+        if libcloud_network:
+            network_object = Network(title=libcloud_network.name,
+                                     libcloud_id=libcloud_network.id,
+                                     cloud=cloud,
+                                     extra=copy.copy(libcloud_network.extra))
+        else:
+            network_object = Network(title=network_info['name'],
+                                     libcloud_id=network_info['id'],
+                                     cloud=cloud,
+                                     extra={key: value for key, value in network_info.items()
+                                            if key not in ['name', 'id']})
+        if do_save:
+            try:
+                network_object.save()
+            except me.ValidationError as exc:
+                log.error("Error adding network: %s: %s", libcloud_network.name, exc.to_dict())
+                raise BadRequestError({"msg": exc.message,
+                                       "errors": exc.to_dict()})
+
+        return network_object
+
 
 class Subnet(me.Document):
     id = me.StringField(primary_key=True, default=lambda: uuid.uuid4().hex)
@@ -47,3 +83,35 @@ class Subnet(me.Document):
                                                                               cloud=self.cloud,
                                                                               cloud_id=self.libcloud_id,
                                                                               parent_network=self.base_network)
+
+    @classmethod
+    def create(cls, cloud, parent_network, libcloud_subnet=None, subnet_info=None, do_save=True):
+        """ Persist a new Subnet object to the DB
+            Should not be overridden or extended by subclasses"""
+
+        if not libcloud_subnet and not subnet_info:
+            raise RequiredParameterMissingError('libcloud_subnet ot subnet_info')
+
+        if libcloud_subnet:
+            subnet_object = Subnet(title=libcloud_subnet.name,
+                                   libcloud_id=libcloud_subnet.id,
+                                   cloud=cloud,
+                                   base_network=parent_network,
+                                   extra=copy.copy(libcloud_subnet.extra))
+        else:
+            subnet_object = Subnet(title=subnet_info['name'],
+                                   libcloud_id=subnet_info['id'],
+                                   cloud=cloud,
+                                   base_network=parent_network,
+                                   extra={key: value for key, value in subnet_info.items()
+                                          if key not in ['name', 'id']})
+
+        if do_save:
+            try:
+                subnet_object.save()
+            except me.ValidationError as exc:
+                log.error("Error adding subnet: %s: %s", libcloud_subnet.name, exc.to_dict())
+                raise BadRequestError({"msg": exc.message,
+                                       "errors": exc.to_dict()})
+
+        return subnet_object
