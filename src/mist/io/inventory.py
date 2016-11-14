06d3e1bd860b4de0c8638ff8131c07b6f3090f84
johnnyWalnut <ikarydis0@gmail.com>
2016-11-14 15:10:17 +0200

    minor change here, for new class SignedSSHKey

--- src/mist/io/inventory.py
+++ src/mist/io/inventory.py
@@ -2,7 +2,7 @@ try:
     from mist.core.user.models import User
     from mist.io.clouds.models import Cloud
     from mist.io.machines.models import Machine, KeyAssociation
-    from mist.io.keys.models import Key
+    from mist.io.keys.models import Key, SignedSSHKey
     from mist.core import config
     from mist.core.vpn.methods import destination_nat as dnat
 except ImportError:
@@ -37,7 +37,7 @@ class MistInventory(object):
             if key_id not in self.keys:
                 keypair = Key.objects.get(owner=self.user, name=key_id)
                 self.keys[key_id] = keypair.private
-                if keypair.certificate:
+                if isinstance(keypair, SignedSSHKey):
                     # if signed ssh key, provide the key appending a -cert.pub
                     # on the name since this is how ssh will include it as
                     # an identify file
