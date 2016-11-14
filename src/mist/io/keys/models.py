7e245f27895e63921d8dfe19809826c0108b8005
johnnyWalnut <ikarydis0@gmail.com>
2016-11-14 18:35:02 +0200

    add docstrings class Key(me.Document) class SSHKey(Key) class SignedSSHKey(SSHKey)

--- src/mist/io/keys/models.py
+++ src/mist/io/keys/models.py
@@ -5,46 +5,146 @@ import mist.core.tag.models
 from Crypto.PublicKey import RSA
 from mist.core.user.models import Owner, Organization
 from mist.io.exceptions import BadRequestError
-from mist.io.keypairs.controllers import KeypairController
+from mist.io.keys import controllers
+from mist.io.keys.base import BaseKeyController
 from mist.io.exceptions import RequiredParameterMissingError
 
 
-# TODO rename sshkeys
-class Keypair(me.Document):
-    """An ssh keypair."""
+class Key(me.Document):
+    """Abstract base class for every key/machine attr mongoengine model.
+
+    This class defines the fields common to all keys of all types. For each
+    different key type, a subclass should be created adding any key
+    specific fields and methods.
+
+    Documents of all Key subclasses will be stored on the same mongo
+    collection.
+
+    One can perform a query directly on Key to fetch all key types, like
+    this:
+
+        Key.objects(owner=owner).count()
+
+    This will return an iterable of keys for that owner. Each key will be
+    an instance of its respective Key subclass, like SSHKey and SignedSSHKey
+    instances.
+
+    Keys of a specific type can be queried like this:
+
+        SSKey.objects(owner=owner).count()
+
+    This will return an iterable of SSHKey instances.
+
+    To create a new key, one should initialize a Key subclass like
+    SSHKey. Initializing directly a Key instance won't have any fields
+    or associated handler to work with.
+
+    Each Key subclass should define a `_controller_cls` class attribute. Its
+    value should be a subclass of
+    `mist.io.keys.controllers.BaseKeyController'. These
+    subclasses are stored in `mist.io.keys.BaseKeyController`. When a key is
+    instanciated, it is given a `ctl` attribute which gives access to the
+    keys controller. This way it is possible to do things like:
+
+        key = Key.objects.get(id=key_id)
+        key.ctl.generate()
+
+    """
+    meta = {
+        'allow_inheritance': True,
+        'collection': 'keypair',
+    }
+
     id = me.StringField(primary_key=True,
                         default=lambda: uuid4().hex)
     name = me.StringField(required=True, unique_with="owner")
+    owner = me.ReferenceField(Owner)  # TODO Organization required?
+
     public = me.StringField(required=True)
     private = me.StringField(required=True)
-    certificate = me.StringField(required=False)
     default = me.BooleanField(default=False)
-    owner = me.ReferenceField(Owner)   # TODO Organization required?
+
+    _controller_cls = None
 
     def __init__(self, *args, **kwargs):
-        super(Keypair, self).__init__(*args, **kwargs)
-        self.ctl = KeypairController(self)
+        super(Key, self).__init__(*args, **kwargs)
+
+        # Set attribute `ctl` to an instance of the appropriate controller.
+        if self._controller_cls is None:
+            raise NotImplementedError(
+                "Can't initialize %s. Key is an abstract base class and "
+                "shouldn't be used to create cloud instances. All Key "
+                "subclasses should define a `_controller_cls` class attribute "
+                "pointing to a `BaseController` subclass." % self
+            )
+        elif not issubclass(self._controller_cls, BaseKeyController):
+            raise TypeError(
+                "Can't initialize %s.  All Key subclasses should define a"
+                " `_controller_cls` class attribute pointing to a "
+                "`BaseController` subclass." % self
+            )
+        self.ctl = self._controller_cls(self)
+
+        # Calculate and store key type specific fields.
+        self._key_specific_fields = [field for field in type(self)._fields
+                                          if field not in Key._fields]
 
     @classmethod
     def add(cls, owner, name, id='', **kwargs):
-        """Add keypair
+        """Add key
+
+        This is a class method, meaning that it is meant to be called on the
+        class itself and not on an instance of the class.
+
+        You 're not meant to be calling this directly, but on a key subclass
+        instead like this:
+
+            key = SSHKey.add(owner=org, name='unicorn', **kwargs)
         """
         if not name:
             raise RequiredParameterMissingError('title')
         if not owner or not isinstance(owner, Organization):
             raise BadRequestError('owner')
-        keypair = cls(owner=owner, name=name)
-        keypair.ctl.add(**kwargs)
-        return keypair
+        # if it already exist?  # TODO
+        # if Key.objects(owner=owner, name=name):
+        #     raise KeyExistsError()
+        key = cls(owner=owner, name=name)
+        key.ctl.add(**kwargs)
+        return key
+
+    def delete(self):
+        super(Key, self).delete()
+        mist.core.tag.models.Tag.objects(resource=self).delete()
+
+    def as_dict(self):
+        # Return a dict as it will be returned to the API
+        mdict = {
+            'id': self.id,
+            'name': self.name,
+            'owner': self.owner.id,
+            'default': self.default,
+            'public': self.public,
+        }
+
+        mdict.update({key: getattr(self, key)
+                      for key in self._key_specific_fields})
+        return mdict
+
+    def __str__(self):
+        return '%s key %s (%s) of %s' % (type(self), self.name,
+                                           self.id, self.owner)
+
+
+class SSHKey(Key):
+    """An ssh key."""
+    meta = {
+        'allow_inheritance': True,
+    }
+
+    _controller_cls = controllers.SSHKeyController
 
     def clean(self):
-        """Checks if certificate is specific ssh-rsa-cert
-           and ensures that self is a valid RSA keypair."""
-        if (self.certificate and
-                not self.certificate.startswith('ssh-rsa-cert-v01@openssh.com'
-                                                )):
-            self.certificate = ''
-
+        """Ensures that self is a valid RSA keypair."""
         from Crypto import Random
         Random.atfork()
         message = 'Message 1234567890'
@@ -57,19 +157,19 @@ class Keypair(me.Document):
                 return True
             raise me.ValidationError("Invalid RSA keypair")  # TODO is it ok?
 
-    def delete(self):
-        super(Keypair, self).delete()
-        mist.core.tag.models.Tag.objects(resource=self).delete()
 
-    def as_dict(self):
-        return {
-            'id': self.id,
-            'name': self.name,
-            'owner': self.owner.id,
-            'public': self.public,
-            'default': self.default,
-            'certificate': self.certificate,
-        }
+class SignedSSHKey(SSHKey):
+    """An signed ssh key"""
+    certificate = me.StringField(required=False)  # TODO True?
 
-    def __str__(self):
-        return 'Keypair %s (%s) of %s' % (self.name, self.id, self.owner)
+    _controller_cls = controllers.BaseKeyController
+
+    def clean(self):
+        """
+        # Checks if certificate is specific ssh-rsa-cert
+           and ensures that self is a valid RSA keypair."""
+        super(SignedSSHKey, self).clean()
+        if (self.certificate and
+                not self.certificate.startswith('ssh-rsa-cert-v01@openssh.com'
+                                                )):
+            self.certificate = ''
