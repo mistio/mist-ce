17b7989b4bcda48b2868586831d173424c98e04f
johnnyWalnut <ikarydis0@gmail.com>
2016-11-21 15:26:20 +0200

    delete commented out codde

--- src/mist/io/keys/base.py
+++ src/mist/io/keys/base.py
@@ -110,17 +110,6 @@ class BaseKeyController(object):
 
         log.info("Setting key with id '%s' as default.", key_id)
 
-        # default_key = Key.objects(owner=self.key.owner,
-        #                           default=True).first()
-        # if default_key:
-        #     default_key.default = False
-        #     default_key.save()
-        #
-        # key = Key.objects.get(owner=self.key.owner, id=key_id)
-        # key.default = True
-        # key.save()
-
-        # TODO do we prefer this?
         Key.objects(owner=self.key.owner, default=True).update(default=False)
         Key.objects(owner=self.key.owner, id=key_id).update(default=True)
 
