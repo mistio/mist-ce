6517cb1408c8ab7ec972ecd79be8ca7445d81629
johnnyWalnut <ikarydis0@gmail.com>
2016-12-01 14:02:53 +0200

    in CollecdController  add def deploy_and_assoc_python_plugin_from_script  and change its attrs, we need only machine instance

--- src/mist/io/scripts/controllers.py
+++ src/mist/io/scripts/controllers.py
@@ -1,3 +1,4 @@
+import re
 import yaml
 import logging
 from mist.io.exceptions import BadRequestError
@@ -7,6 +8,7 @@ from yaml.parser import ParserError as YamlParserError
 from yaml.scanner import ScannerError as YamlScannerError
 
 log = logging.getLogger(__name__)
+
 
 class AnsibleScriptController(BaseScriptController):
 
@@ -30,4 +32,32 @@ class ExecutableScriptController(BaseScriptController):
                 )
 
 
-# class CollectdScript(BaseScriptController):
+class CollectdScriptController(ExecutableScriptController):
+
+    def deploy_and_assoc_python_plugin_from_script(self, machine):
+        from mist.core.methods import assoc_metric, update_metric
+        from mist.io.methods import deploy_python_plugin
+        from mist.io.scripts.models import CollectdScript
+
+        if not isinstance(self.script, CollectdScript):
+            raise BadRequestError("Script should have exec_type of "
+                                  "'collectd_python_plugin'.")
+
+        # Construct plugin_id from script name
+        plugin_id = self.script.name.lower()
+        plugin_id = re.sub('[^a-z0-9_]+', '_', plugin_id)
+        plugin_id = re.sub('_+', '_', plugin_id)
+        plugin_id = re.sub('^_', '', plugin_id)
+        plugin_id = re.sub('_$', '', plugin_id)
+        ret = deploy_python_plugin(
+            self.script.owner, machine.cloud.id, machine.machine_id, plugin_id,
+            self.script.extra.get('value_type', 'gauge'), self.script.script,
+            machine.hostname
+        )
+        metric_id = ret['metric_id']
+        update_metric(self.script.owner, metric_id=metric_id,
+                      name=self.script.name,
+                      unit=self.script.extra.get('value_unit', ''))
+        assoc_metric(self.script.owner, machine.cloud.id, machine.machine_id,
+                     metric_id)
+        return ret
