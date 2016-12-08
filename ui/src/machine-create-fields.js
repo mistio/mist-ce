878b2efd38e546aa3da2b20b154625287338f83a
xristina <xristina@mist.io>
2016-12-08 21:57:21 +0200

    typo

--- ui/src/machine-create-fields.js
+++ ui/src/machine-create-fields.js
@@ -273,7 +273,7 @@ MACHINE_CREATE_FIELDS.forEach(function(p){
     });
 
     //add cloud init only to providers that accept and we support
-    if (['azure', 'digitalocean', 'ec2', 'gce', 'packet', 'rackspace', 'libvirt'].indexOf(p.provider) == -1) {
+    if (['azure', 'digitalocean', 'ec2', 'gce', 'packet', 'rackspace', 'libvirt'].indexOf(p.provider) != -1) {
         p.fields.push({
             name: "cloud_init",
             label: "Cloud Init",
