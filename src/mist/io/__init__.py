894c80704c5ec14efe7a8a6bbdd63dfe027ad8a6
johnnyWalnut <ikarydis0@gmail.com>
2017-02-13 20:22:37 +0200

    transfer tag's  routes from core

--- src/mist/io/__init__.py
+++ src/mist/io/__init__.py
@@ -82,6 +82,24 @@ def add_routes(configurator):
     configurator.add_route('api_v1_machine_tag',
                            '/api/v1/clouds/{cloud}/machines/{machine}/tags/{tag}')
     configurator.add_route('api_v1_tags', '/api/v1/tags')
+    configurator.add_route('cloud_tags', '/clouds/{cloud_id}/tags')
+    configurator.add_route('key_tags', '/keys/{key_id}/tags')
+
+    configurator.add_route('script_tags', '/scripts/{script_id}/tags')
+    configurator.add_route('schedule_tags', '/schedules/{schedule_id}/tags')
+    configurator.add_route('network_tags',
+                           '/clouds/{cloud_id}/networks/{network_id}/tags')
+
+    configurator.add_route('script_tag', '/scripts/{script_id}/tag')
+    configurator.add_route('schedule_tag', '/schedules/{schedule_id}/tag')
+    configurator.add_route('network_tag',
+                           '/clouds/{cloud}/networks/{network_id}/tag/{tag_key}')
+    configurator.add_route('key_tag', '/keys/{key_id}/tag')
+    configurator.add_route('cloud_tag', '/clouds/{cloud_id}/tag')
+
+    configurator.add_route('machine_tag',
+                           '/clouds/{cloud_id}/machines/{machine_id}/tag')
+
     configurator.add_route('api_v1_probe',
                            '/api/v1/clouds/{cloud}/machines/{machine}/probe')
 
