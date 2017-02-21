94b9e667c3864dc430b7aff1dc9ad8bed4520ab9
spiros <spirostza77@hotmail.com>
2017-02-22 00:19:44 +0200

    pep8 fix

--- src/mist/io/keys/views.py
+++ src/mist/io/keys/views.py
@@ -357,8 +357,8 @@ def associate_key(request):
     auth_context = auth_context_from_request(request)
 
     try:
-       Cloud.objects.get(owner=auth_context.owner,
-                                  id=cloud_id, deleted=None)
+        Cloud.objects.get(owner=auth_context.owner,
+                          id=cloud_id, deleted=None)
     except Cloud.DoesNotExist:
         raise NotFoundError('Cloud does not exist')
 
