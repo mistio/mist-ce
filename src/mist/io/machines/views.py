b5b15aadd56c87d3884e67d9511fe25f44f797ec
spiros <spirostza77@hotmail.com>
2017-02-22 00:29:48 +0200

    fuxkin pep8 fix

--- src/mist/io/machines/views.py
+++ src/mist/io/machines/views.py
@@ -236,8 +236,8 @@ def create_machine(request):
     auth_context = auth_context_from_request(request)
 
     try:
-        cloud = Cloud.objects.get(owner=auth_context.owner,
-                                  id=cloud_id, deleted=None)
+        Cloud.objects.get(owner=auth_context.owner,
+                          id=cloud_id, deleted=None)
     except Cloud.DoesNotExist:
         raise NotFoundError('Cloud does not exist')
 
