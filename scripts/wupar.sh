b02ab38889999834d490c1fd50ac7f0cb01142fc
Dimitris Rozakis <dimrozakis@gmail.com>
2018-05-21 22:11:24 +0300

    Axtensive changes in CI, deployment etc
    to bring mist & clearglass repos closer

--- scripts/wupar.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# [w]ait [u]ntil [p]ods [a]re [r]unning
-
-NAMESPACE="${1:-default}"
-echo $NAMESPACE
-until !( kubectl --namespace $NAMESPACE get pods | grep -v NAME | grep -v Running ); do
-    sleep 5 && echo '.'
-done
