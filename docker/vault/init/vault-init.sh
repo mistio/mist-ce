4b80fa86c904bf1750c83b8471ae603862e7c350
Dimitris Moraitis <dimo@mist.io>
2021-04-11 03:26:00 +0300

    Always unseal & login to vault on start

--- docker/vault/init/vault-init.sh
+++ docker/vault/init/vault-init.sh
@@ -14,8 +14,8 @@ ROLE_TOKEN_PATH=/approle/token
 
 if [ -f "$UNSEAL_TOKEN_PATH" ]; then
     echo "Unsealing vault"
-	socat STDIO 'EXEC:vault operator unseal,PTY' < $UNSEAL_TOKEN_PATH
-    vault login $ROOT_TOKEN
+    socat STDIO 'EXEC:vault operator unseal,PTY' < $UNSEAL_TOKEN_PATH
+    socat STDIO 'EXEC:vault login,PTY' < $ROOT_TOKEN_PATH
     vault policy write mist /vault/policies/vault-mist.hcl
     vault write auth/approle/role/mist token_num_uses=0 token_policies=mist
     ROLE_ID=$(vault read auth/approle/role/mist/role-id -format=json |jq .data.role_id -r)
@@ -23,16 +23,16 @@ if [ -f "$UNSEAL_TOKEN_PATH" ]; then
     ROLE_TOKEN=$(vault write auth/approle/login secret_id=$SECRET_ID role_id=$ROLE_ID -format=json | jq .auth.client_token -r)
     echo $ROLE_TOKEN > $ROLE_TOKEN_PATH
 else
-	echo "Initializing Vault server"
-	vault operator init -key-shares=1 -key-threshold=1 -format=json > "$INIT_LOG"
-	export UNSEAL_TOKEN=$(cat $INIT_LOG | jq .unseal_keys_b64[0] -r)
+    echo "Initializing Vault server"
+    vault operator init -key-shares=1 -key-threshold=1 -format=json > "$INIT_LOG"
+    export UNSEAL_TOKEN=$(cat $INIT_LOG | jq .unseal_keys_b64[0] -r)
     export ROOT_TOKEN=$(cat $INIT_LOG | jq .root_token -r)
     echo "$ROOT_TOKEN" > "$ROOT_TOKEN_PATH"
-	echo "$UNSEAL_TOKEN" > "$UNSEAL_TOKEN_PATH"
-	vault operator unseal $UNSEAL_TOKEN
-	vault login $ROOT_TOKEN
-	vault secrets enable -path="kv1" kv
-	vault policy write mist /vault/policies/vault-mist.hcl
+    echo "$UNSEAL_TOKEN" > "$UNSEAL_TOKEN_PATH"
+    socat STDIO 'EXEC:vault operator unseal,PTY' < $UNSEAL_TOKEN_PATH
+    socat STDIO 'EXEC:vault login,PTY' < $ROOT_TOKEN_PATH
+    vault secrets enable -path="kv1" kv
+    vault policy write mist /vault/policies/vault-mist.hcl
     vault auth enable approle
     vault write auth/approle/role/mist token_num_uses=0 token_policies=mist
     ROLE_ID=$(vault read auth/approle/role/mist/role-id -format=json |jq .data.role_id -r)
