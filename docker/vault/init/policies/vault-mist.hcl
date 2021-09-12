bb55836862b94148922e4a9478922be663374e44
Dimitris Moraitis <dimo@mist.io>
2021-09-12 23:23:45 +0300

    Use AppRole auth by default, update default policy

--- docker/vault/init/policies/vault-mist.hcl
+++ docker/vault/init/policies/vault-mist.hcl
@@ -1,19 +1,3 @@
-# Grant permissions on kv1 engine
 path "+/*" {
     capabilities = [ "create", "update", "read", "delete", "list" ]
-}
-
-# For Web UI usage
-path "kv1/metadata" {
-  capabilities = ["list"]
-}
-
-# Grant permissions on kv2 engine
-path "secret/+/*" {
-    capabilities = [ "create", "update", "read", "delete", "list" ]
-}
-
-# For Web UI usage
-path "secret/metadata" {
-  capabilities = ["list"]
 }
--- docker/vault/init/vault-init.sh
+++ docker/vault/init/vault-init.sh
@@ -10,7 +10,9 @@ fi
 INIT_LOG=/vault/init.log
 UNSEAL_TOKEN_PATH=/vault/config/unseal_token
 ROOT_TOKEN_PATH=/vault/config/root_token
-ROLE_TOKEN_PATH=/approle/token
+# ROLE_TOKEN_PATH=/approle/token
+ROLE_ID_PATH=/approle/role_id
+SECRET_ID_PATH=/approle/secret_id
 
 if [ -f "$UNSEAL_TOKEN_PATH" ]; then
     echo "Unsealing vault"
@@ -20,8 +22,12 @@ if [ -f "$UNSEAL_TOKEN_PATH" ]; then
     vault write auth/approle/role/mist token_num_uses=0 token_policies=mist
     ROLE_ID=$(vault read auth/approle/role/mist/role-id -format=json |jq .data.role_id -r)
     SECRET_ID=$(vault write -f auth/approle/role/mist/secret-id -format=json | jq .data.secret_id -r)
-    ROLE_TOKEN=$(vault write auth/approle/login secret_id=$SECRET_ID role_id=$ROLE_ID -format=json | jq .auth.client_token -r)
-    echo $ROLE_TOKEN > $ROLE_TOKEN_PATH
+    # ROLE_TOKEN=$(vault write auth/approle/login secret_id=$SECRET_ID role_id=$ROLE_ID -format=json | jq .auth.client_token -r)
+    # echo $ROLE_TOKEN > $ROLE_TOKEN_PATH
+    echo $ROLE_ID > $ROLE_ID_PATH
+    echo "Role Id: $ROLE_ID"
+    echo $SECRET_ID > $SECRET_ID_PATH
+    echo "Secref Id: $SECRET_ID"
 else
     echo "Initializing Vault server"
     vault operator init -key-shares=1 -key-threshold=1 -format=json > "$INIT_LOG"
@@ -37,8 +43,12 @@ else
     vault write auth/approle/role/mist token_num_uses=0 token_policies=mist
     ROLE_ID=$(vault read auth/approle/role/mist/role-id -format=json |jq .data.role_id -r)
     SECRET_ID=$(vault write -f auth/approle/role/mist/secret-id -format=json | jq .data.secret_id -r)
-    ROLE_TOKEN=$(vault write auth/approle/login secret_id=$SECRET_ID role_id=$ROLE_ID -format=json | jq .auth.client_token -r)
-    echo $ROLE_TOKEN > $ROLE_TOKEN_PATH
+    # ROLE_TOKEN=$(vault write auth/approle/login secret_id=$SECRET_ID role_id=$ROLE_ID -format=json | jq .auth.client_token -r)
+    # echo $ROLE_TOKEN > $ROLE_TOKEN_PATH
+    echo $ROLE_ID > $ROLE_ID_PATH
+    echo "Role Id: $ROLE_ID"
+    echo $SECRET_ID > $SECRET_ID_PATH
+    echo "Secref Id: $SECRET_ID"
 fi
 echo "Done!"
 
