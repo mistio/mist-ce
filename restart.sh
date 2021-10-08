920d585584cfb418e3f3dc88fabcdf549a6f8c3c
Dimitris Moraitis <dimo@mist.io>
2021-10-08 13:58:15 +0300

    Reload code in api & dramatiq by default

--- restart.sh
+++ restart.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 if [ $# -eq 0 ]; then
+    echo "Reloading api & dramatiq"
+    echo "---------------------------------------------------"
+    docker-compose exec api sh -c "kill -HUP 1"
+    echo "api-v1 \tDone"
+    docker-compose exec api-v2 sh -c "kill -HUP 1"
+    echo "api-v2 \tDone"
+    docker-compose exec dramatiq bash -c 'kill -HUP $(ps aux | grep -e python | awk "{ print \$2 }")'
+    echo "dramatiq \tDone"
+    echo "Done."
+elif [ $# -eq 1 ] && [ "$@" = "all" ]; then
     echo "Restarting all containers that import mist.api code"
     echo "---------------------------------------------------"
     docker-compose restart api api-v2 dramatiq sockjs scheduler
@@ -15,7 +25,7 @@ elif [ $# -eq 1 ] && [ "$@" = "api" ]; then
 elif [ $# -eq 1 ] && [ "$@" = "dramatiq" ]; then
     echo "Sending HUP signal to dramatiq"
     echo "---------------------------------------------------"
-    docker-compose exec dramatiq sh -c "pkill -HUP python"
+    docker-compose exec dramatiq bash -c 'kill -HUP $(ps aux | grep -e python | awk "{ print \$2 }")'
     echo "Done"
 else
     echo "Restarting $@"
